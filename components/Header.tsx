6aa31eae0b1063e1d679721dea64dfdcb7c984f5
Saul Paredes <saulkurosaki13@gmail.com>
2024-02-20 19:58:12 -0600

    feat(Header): implement the Sidebar component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 
+import SideBar from "./SideBar";
+
 const Header = () => {
   return (
     <nav className="bg-[#B99253] flex h-14 w-full px-4 py-2">
       <div className="container mx-[398.5px] px-[15px] flex justify-between items-center">
-        <div>Hello</div>
+        <SideBar />
+
         <div>Hola</div>
+
         <div>Hi Mates</div>
       </div>
     </nav>

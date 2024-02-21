fc7de0107b384a4c41cc26580e8afa92e0d7138c
Saul Paredes <saulkurosaki13@gmail.com>
2024-02-20 19:54:49 -0600

    feat(Sidebar): implement the shadcn sheet component

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,7 +1,29 @@
 import React from "react";
 
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+
 const SideBar = () => {
-  return <div>SideBar component</div>;
+  return (
+    <Sheet>
+      <SheetTrigger>Open</SheetTrigger>
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle>Are you absolutely sure?</SheetTitle>
+          <SheetDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </SheetDescription>
+        </SheetHeader>
+      </SheetContent>
+    </Sheet>
+  );
 };
 
 export default SideBar;

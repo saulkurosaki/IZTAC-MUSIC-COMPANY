1e0439fcd840a00a913d2a936401fc3000cd8ca7
Saul Paredes <saulkurosaki13@gmail.com>
2024-02-20 19:23:50 -0600

    create a route group called (root)

---
+++ app/(root)/page.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <div className="bg-blue-400">Iztac Music Company</div>;
+}
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer component</div>;
+  return <div className="bg-blue-400">Footer component</div>;
 };
 
 export default Footer;

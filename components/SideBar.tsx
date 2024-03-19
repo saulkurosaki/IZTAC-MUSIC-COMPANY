eb46882da43aa916b09be4b0425730dd61c69398
Saul Paredes <saulkurosaki13@gmail.com>
2024-03-18 20:09:11 -0600

    feat(Sidebar): implement the sheet trigger div responsive layout

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 import {
   Sheet,
   SheetContent,
@@ -9,10 +8,23 @@ import {
   SheetTrigger,
 } from "@/components/ui/sheet";
 
+import hamburger from "../assets/hamburger.png";
+import Image from "next/image";
+
 const SideBar = () => {
   return (
     <Sheet>
-      <SheetTrigger>Open</SheetTrigger>
+      <SheetTrigger>
+        <div className="flex lg:w-[91px] lg:h-[37.5px] justify-center items-center">
+          {/* <Image
+            src={hamburger}
+            alt="hamburger"
+            width={25}
+            height={25}
+            className="opacity-65"
+          /> */}
+        </div>
+      </SheetTrigger>
       <SheetContent>
         <SheetHeader>
           <SheetTitle>Are you absolutely sure?</SheetTitle>

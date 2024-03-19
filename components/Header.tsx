d1edca947db254710b1fdc8a17ed20aa5bd1b1f7
Saul Paredes <saulkurosaki13@gmail.com>
2024-03-18 20:11:10 -0600

    feat(Header): implement the sidebar container responsive layout

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,9 +9,9 @@ const Header = () => {
   return (
     <nav className="bg-[#B99253] flex max-md:h-14 h-[72px] lg:h-14 w-full px-[16px] py-[8px]">
       <div className="w-full md:mx-[8px] lg:mx-[16px] md:py-[8px] xl:mx-[44px] px-[15px] flex justify-between items-center">
-        {/* <div className="hidden lg:flex">
+        <div className="hidden lg:flex">
           <SideBar />
-        </div> */}
+        </div>
 
         {/* <Image src={iztacLogo} alt="logo" width={45} height={45} /> */}
 

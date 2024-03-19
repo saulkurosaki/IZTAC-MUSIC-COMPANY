933b8772796533d0f1f0295a8f1149d6f9e5c87c
Saul Paredes <saulkurosaki13@gmail.com>
2024-03-18 20:10:41 -0600

    feat(Header): implement the navbar container responsive layout

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,17 +3,25 @@ import React from "react";
 import SideBar from "./SideBar";
 import Image from "next/image";
 
-import iztacLogo from "../assets/iztac_logo_black.png";
+import iztacLogo from "../assets/iztac_logo_white.png";
 
 const Header = () => {
   return (
-    <nav className="bg-[#B99253] flex h-14 w-full px-4 py-2">
-      <div className="container mx-[398.5px] px-[15px] flex justify-between items-center">
-        <SideBar />
+    <nav className="bg-[#B99253] flex max-md:h-14 h-[72px] lg:h-14 w-full px-[16px] py-[8px]">
+      <div className="w-full md:mx-[8px] lg:mx-[16px] md:py-[8px] xl:mx-[44px] px-[15px] flex justify-between items-center">
+        {/* <div className="hidden lg:flex">
+          <SideBar />
+        </div> */}
 
-        <Image src={iztacLogo} alt="logo" width={55} height={55} />
+        {/* <Image src={iztacLogo} alt="logo" width={45} height={45} /> */}
 
-        <div>IDENTIFY</div>
+        {/* <div className="hidden lg:flex w-[91px] justify-center items-center">
+          <p className="text-[18px] font-medium text-[#2b2f35]">Conócenos</p>
+        </div> */}
+
+        {/* <div className="flex lg:hidden">
+          <SideBar />
+        </div> */}
       </div>
     </nav>
   );

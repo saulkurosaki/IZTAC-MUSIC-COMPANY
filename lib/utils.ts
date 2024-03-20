c47626a86f64aecdb43912304a2df439e86cbfbe
Saul Paredes <saulkurosaki13@gmail.com>
2024-03-19 22:03:46 -0600

    feat(utils.ts): implement the windowSize state for the useWindowSize custom function

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,36 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { useEffect, useState } from "react";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const useWindowSize = () => {
+  // Initialize state with the width/height to 0 so server and client renders match
+  const [windowSize, setWindowSize] = useState({
+    width: 0,
+    height: 0,
+  });
+
+  // useEffect(() => {
+  //   // Handler to call on window resize
+  //   // function handleResize() {
+  //   //   // Set window width/height to state
+  //   //   setWindowSize({
+  //   //     width: window.innerWidth,
+  //   //     height: window.innerHeight,
+  //   //   });
+  //   // }
+
+  //   // Add event listener
+  //   // window.addEventListener("resize", handleResize);
+
+  //   // Call handler right away so state gets updated with initial window size
+  //   // handleResize();
+
+  //   // Remove event listener on cleanup
+  //   // return () => window.removeEventListener("resize", handleResize);
+  // }, []);
+  return windowSize;
+};

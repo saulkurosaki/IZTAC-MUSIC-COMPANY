1dc2a1e52dbb524e1b09fda2405e9ec0ab4277ff
Saul Paredes <saulkurosaki13@gmail.com>
2024-03-19 22:04:52 -0600

    feat(utils.ts): initialize a useEffect and implement the setWindowSize function for the useWindowSize utility function

--- lib/utils.ts
+++ lib/utils.ts
@@ -13,24 +13,24 @@ export const useWindowSize = () => {
     height: 0,
   });
 
-  // useEffect(() => {
-  //   // Handler to call on window resize
-  //   // function handleResize() {
-  //   //   // Set window width/height to state
-  //   //   setWindowSize({
-  //   //     width: window.innerWidth,
-  //   //     height: window.innerHeight,
-  //   //   });
-  //   // }
+  useEffect(() => {
+    // Handler to call on window resize
+    function handleResize() {
+      // Set window width/height to state
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
 
-  //   // Add event listener
-  //   // window.addEventListener("resize", handleResize);
+    // Add event listener
+    // window.addEventListener("resize", handleResize);
 
-  //   // Call handler right away so state gets updated with initial window size
-  //   // handleResize();
+    // Call handler right away so state gets updated with initial window size
+    // handleResize();
 
-  //   // Remove event listener on cleanup
-  //   // return () => window.removeEventListener("resize", handleResize);
-  // }, []);
+    // Remove event listener on cleanup
+    // return () => window.removeEventListener("resize", handleResize);
+  }, []);
   return windowSize;
 };

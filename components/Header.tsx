49effc257a7921ea8196ff23ff5b9029e43e2d1b
Saul Paredes <saulkurosaki13@gmail.com>
2024-03-20 19:28:55 -0600

    feat(Header): make the navbar sticky to the website

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,7 +8,7 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <nav className="bg-[#B99253] flex max-md:h-14 h-[72px] lg:h-14 w-full px-[16px] py-[8px]">
+    <nav className="bg-[#B99253] flex max-md:h-14 h-[72px] lg:h-14 w-full px-[16px] py-[8px] sticky">
       <div className="flex w-full md:mx-[8px] lg:mx-[16px] xl:mx-[44px] 2xl:mx-[604px] md:py-[8px] px-[15px] justify-between items-center">
         <div className="hidden lg:flex">
           <SideBar />

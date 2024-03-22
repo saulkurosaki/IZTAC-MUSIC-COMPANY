890535af91ece50b5d543811b6f583fb7cb64c3e
Saul Paredes <saulkurosaki13@gmail.com>
2024-03-21 22:03:54 -0600

    feat(HeroBanner): implement the shadcn Carousel component useEffect logic to keep track and set the current carousel item state

--- components/HeroBanner.tsx
+++ components/HeroBanner.tsx
@@ -21,18 +21,18 @@ const HeroBanner = () => {
   const [current, setCurrent] = useState(0);
   const [count, setCount] = useState(0);
 
-  //   useEffect(() => {
-  //     if (!api) {
-  //       return;
-  //     }
+  useEffect(() => {
+    if (!api) {
+      return;
+    }
 
-  //     setCount(api.scrollSnapList().length);
-  //     setCurrent(api.selectedScrollSnap() + 1);
+    setCount(api.scrollSnapList().length);
+    setCurrent(api.selectedScrollSnap() + 1);
 
-  //     api.on("select", () => {
-  //       setCurrent(api.selectedScrollSnap() + 1);
-  //     });
-  //   }, [api]);
+    api.on("select", () => {
+      setCurrent(api.selectedScrollSnap() + 1);
+    });
+  }, [api]);
 
   return (
     <>

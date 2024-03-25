93f5206e7c81a40836bfca054e9787f08870d5a2
Saul Paredes <saulkurosaki13@gmail.com>
2024-03-25 17:34:06 -0600

    feat(HeroBanner): implement the first hero banner image inner text layout

--- components/HeroBanner.tsx
+++ components/HeroBanner.tsx
@@ -16,6 +16,7 @@ import Banner_1 from "../assets/banner_image_1.png";
 import Banner_2 from "../assets/banner_image_2.webp";
 import Banner_3 from "../assets/banner_image_3.jpg";
 import CarouselTracker from "./CarouselTracker";
+import Link from "next/link";
 
 const HeroBanner = () => {
   const [api, setApi] = useState<CarouselApi>();
@@ -49,13 +50,28 @@ const HeroBanner = () => {
         className="bg-slate-600 h-[770px]"
       >
         <CarouselContent className="-ml-0">
-          <CarouselItem className={`pl-0`}>
+          <CarouselItem className="pl-0">
             <Image
               src={Banner_1}
               alt="banner_image_1"
-              className="w-full h-[770px] object-cover overflow-hidden z-10"
+              className="w-full h-[770px] object-cover z-10"
             />
-            <div className="w-[1290px] h-[466px] bg-slate-600 opacity-50 z-20"></div>
+            <div className="absolute inset-0 flex justify-center items-center">
+              <div className="flex flex-col justify-center items-center w-[720px] h-[241px] text-white z-20">
+                {/* <h2 className="text-[65px] font-oxygen">Bienvenido a IMC</h2>
+                <p className="pb-4 text-3xl font-oxygen">Iztac Music Company</p>
+                <p className="pb-4 text-xl font-oxygen">
+                  Somos una productora musical especializada en cine, televisión
+                  y videojuegos
+                </p> */}
+                {/* <Link
+                  href="/about-us"
+                  className="text-[23px] font-normal mt-auto flex justify-center items-center w-44 h-12 bg-[#B99253] rounded-lg text-[#2b2f35ef] px-4 pya-2"
+                >
+                  Descubre más
+                </Link> */}
+              </div>
+            </div>
           </CarouselItem>
           <CarouselItem className="pl-0">
             <Image

b367db831e6e3ffe2271e32b8c4278dfca46ba56
Saul Paredes <saulkurosaki13@gmail.com>
2024-03-27 19:49:58 -0600

    feat(utils.ts): implement the generateClipPathTopStyle utility function initial layout

--- lib/utils.ts
+++ lib/utils.ts
@@ -34,3 +34,31 @@ export const useWindowSize = () => {
   }, []);
   return windowSize;
 };
+
+export const generateClipPathTopStyle = (mediaQuery: number) => {
+  // if (mediaQuery <= 768) {
+  //   return "polygon(0 0, 100% 0, 100% 28%, 0 58%)";
+  // } else if (mediaQuery <= 1024) {
+  //   return "polygon(0 0, 100% 0, 100% 22%, 0 62%)";
+  // } else if (mediaQuery <= 1280) {
+  //   return "polygon(0 0, 100% 0, 100% 19%, 0 66%)";
+  // } else if (mediaQuery <= 1536) {
+  //   return "polygon(0 0, 100% 0, 100% 14%, 0 71%)";
+  // } else {
+  //   return "polygon(0 0, 100% 0, 100% 0, 0 100%)";
+  // }
+};
+
+// export const generateClipPathBottomStyle = (mediaQuery: number) => {
+//   if (mediaQuery <= 768) {
+//     return "polygon(0 52%, 100% 83%, 100% 100%, 0 100%)";
+//   } else if (mediaQuery <= 1024) {
+//     return "polygon(0 28%, 100% 69%, 100% 100%, 0 100%)";
+//   } else if (mediaQuery <= 1280) {
+//     return "polygon(0 31%, 100% 82%, 100% 100%, 0 100%)";
+//   } else if (mediaQuery <= 1536) {
+//     return "polygon(0 26%, 100% 87%, 100% 100%, 0 100%)";
+//   } else {
+//     return "polygon(0 0, 100% 100%, 100% 100%, 0 100%)";
+//   }
+// };

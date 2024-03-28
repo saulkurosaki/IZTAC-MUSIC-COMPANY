4c2b741ebc918e11459bca6e995d6b0913de23b8
Saul Paredes <saulkurosaki13@gmail.com>
2024-03-27 21:07:53 -0600

    feat(HeroBanner): implement the banner 2 on large devices responsive design

--- components/HeroBanner.tsx
+++ components/HeroBanner.tsx
@@ -114,8 +114,8 @@ const HeroBanner = () => {
               className="w-full h-[770px] object-cover z-10"
             />
             <div className="w-full absolute left-[100%] inset-0 flex justify-center items-center">
-              <div className="flex flex-col justify-center items-center w-[1000px] h-[198px] text-white z-20">
-                <h2 className="text-[55px] oxygen">
+              <div className="flex flex-col justify-center items-center w-[1000px] h-44 2xl:h-[198px] text-white z-20">
+                <h2 className="text-[55px] max-xl:text-5xl max-xl:pb-5 oxygen">
                   Lleva tu proyecto al siguiente nivel
                 </h2>
                 <p className="pb-4 text-xl oxygen">

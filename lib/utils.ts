98c7434aa39fb1bb521867a190733f8a1fd3f5a0
Saul Paredes <saulkurosaki13@gmail.com>
2024-03-27 19:54:57 -0600

    feat(utils.ts): implement the generateClipPathBottomStyle utility function on max-width of 2560px polygon styles

--- lib/utils.ts
+++ lib/utils.ts
@@ -50,15 +50,15 @@ export const generateClipPathTopStyle = (mediaQuery: number) => {
 };
 
 export const generateClipPathBottomStyle = (mediaQuery: number) => {
-    if (mediaQuery <= 768) {
-      return "polygon(0 52%, 100% 83%, 100% 100%, 0 100%)";
-    } else if (mediaQuery <= 1024) {
-      return "polygon(0 28%, 100% 69%, 100% 100%, 0 100%)";
-    } else if (mediaQuery <= 1280) {
-      return "polygon(0 31%, 100% 82%, 100% 100%, 0 100%)";
-    } else if (mediaQuery <= 1536) {
-      return "polygon(0 26%, 100% 87%, 100% 100%, 0 100%)";
-  //   } else {
-  //     return "polygon(0 0, 100% 100%, 100% 100%, 0 100%)";
-  //   }
+  if (mediaQuery <= 768) {
+    return "polygon(0 52%, 100% 83%, 100% 100%, 0 100%)";
+  } else if (mediaQuery <= 1024) {
+    return "polygon(0 28%, 100% 69%, 100% 100%, 0 100%)";
+  } else if (mediaQuery <= 1280) {
+    return "polygon(0 31%, 100% 82%, 100% 100%, 0 100%)";
+  } else if (mediaQuery <= 1536) {
+    return "polygon(0 26%, 100% 87%, 100% 100%, 0 100%)";
+  } else {
+    return "polygon(0 0, 100% 100%, 100% 100%, 0 100%)";
+  }
 };

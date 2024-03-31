c705e5059480a6f4b236263efeba3142a686e525
Saul Paredes <saulkurosaki13@gmail.com>
2024-03-30 20:08:06 -0600

    feat(AboutUsSection): implement the component background image

--- components/AboutUsSection.tsx
+++ components/AboutUsSection.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const AboutUsSection = () => {
-  return <div>AboutUsSection component Test c:</div>;
+  return (
+    <div className="w-full h-[727px] pt-8 pb-32 about-us-sec-img no bg-no-repeat bg-bottom bg-cover">
+      {/* <Image
+        src={About_Us_Image}
+        alt="Background Image"
+        className="w-full h-full"
+      /> */}
+    </div>
+  );
 };
 
 export default AboutUsSection;

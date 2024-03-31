9a64e2fce6094d727dca57b3a1ef073a069c014a
Saul Paredes <saulkurosaki13@gmail.com>
2024-03-30 20:07:35 -0600

    feat(Home): implement the AboutUsSection component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,12 @@
+import AboutUsSection from "@/components/AboutUsSection";
 import HeroBanner from "@/components/HeroBanner";
 
 export default function Home() {
   return (
     <div className="bg-[#2e2414] w-full h-full">
       <HeroBanner />
+
+      <AboutUsSection />
     </div>
   );
 }

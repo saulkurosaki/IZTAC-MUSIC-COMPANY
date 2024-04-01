daa72a84e975d5ba7ef5ac28926fc7f77dc0359f
Saul Paredes <saulkurosaki13@gmail.com>
2024-03-31 20:33:47 -0600

    feat(AboutUsSection): implement the about us section on large devices responsive layout

--- components/AboutUsSection.tsx
+++ components/AboutUsSection.tsx
@@ -6,14 +6,15 @@ import Logo from "../assets/iztac_logo_white.png";
 
 const AboutUsSection = () => {
   return (
-    <div className="w-full h-[727px] pt-8 pb-32 about-us-sec-img no bg-no-repeat bg-bottom bg-cover flex justify-center items-center">
-      <div className="container w-[1320px] h-[567px] p-0">
+    <div className="w-full h-[727px] max-xl:h-[572px] pt-8 pb-32 about-us-sec-img no bg-no-repeat bg-bottom bg-cover flex justify-center items-center">
+      <div className="container xl:w-[1320px] max-xl:w-[960px] xl:h-[567px] max-xl:h-[412px] p-0">
         <div className="flex w-full h-full items-center">
-          <div className="w-[660px] h-[250px] px-[15px] py-4">
-            <h2 className="oxygen h-[53px] mb-11 font-medium text-white text-[44px] pl-2 flex items-center">
+          {/* Info Section */}
+          <div className="xl:w-[660px] max-xl:w-[480px] xl:h-[250px] max-xl:h-[271px] px-[15px] py-4">
+            <h2 className="oxygen h-[53px] xl:mb-11 max-xl:mb-12 font-medium text-white text-[44px] pl-2 flex items-center">
               Sobre Nosotros
             </h2>
-            <p className="text-white text-lg h-12 mb-5 oxygen flex items-center">
+            <p className="text-white xl:text-lg max-xl:text-base xl:h-12 max-xl:h-[72px] mb-5 oxygen flex items-center">
               IMC es una compañía enfocada en la elaboración de música para
               cine, televisión y videojuegos. ¡Conoce más acerca de nosotros
               aquí!
@@ -27,7 +28,9 @@ const AboutUsSection = () => {
               </Link>
             </div>
           </div>
-          <div className="w-[550px] h-full ml-[110px] px-[15px] py-4">
+
+          {/* Logo */}
+          <div className="xl:w-[550px] max-xl:w-[400px] h-full xl:ml-[110px] max-xl:ml-[80px] px-[15px] py-4">
             <Image src={Logo} alt="logo" className="w-full h-full" />
           </div>
         </div>

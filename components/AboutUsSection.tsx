ee4b2685a6d5367d996cb4f039c27f92493541d3
Saul Paredes <saulkurosaki13@gmail.com>
2024-03-31 21:02:59 -0600

    feat(AboutUsSection): implement the about us section on medium devices responsive layout

--- components/AboutUsSection.tsx
+++ components/AboutUsSection.tsx
@@ -6,15 +6,15 @@ import Logo from "../assets/iztac_logo_white.png";
 
 const AboutUsSection = () => {
   return (
-    <div className="w-full h-[727px] max-xl:h-[572px] pt-8 pb-32 about-us-sec-img no bg-no-repeat bg-bottom bg-cover flex justify-center items-center">
-      <div className="container xl:w-[1320px] max-xl:w-[960px] xl:h-[567px] max-xl:h-[412px] p-0">
-        <div className="flex w-full h-full items-center">
+    <div className="w-full xl:h-[727px] lg:h-[572px] md:h-[1143px] pt-8 pb-32 about-us-sec-img no bg-no-repeat bg-bottom bg-cover max-lg:bg-contain max-md:bg-none flex justify-center items-center">
+      <div className="container xl:w-[1320px] lg:w-[960px] md:w-[720px] xl:h-[567px] lg:h-[412px] md:h-[983px] p-0">
+        <div className="flex w-full h-full items-center max-lg:flex-col">
           {/* Info Section */}
-          <div className="xl:w-[660px] max-xl:w-[480px] xl:h-[250px] max-xl:h-[271px] px-[15px] py-4">
-            <h2 className="oxygen h-[53px] xl:mb-11 max-xl:mb-12 font-medium text-white text-[44px] pl-2 flex items-center">
+          <div className="xl:w-[660px] lg:w-[480px] md:w-[720px] xl:h-[250px] lg:h-[271px] md:h-[242px] px-[15px] py-4">
+            <h2 className="oxygen lg:h-[53px] md:h-11 xl:mb-11 lg:mb-12 md:mb-10 font-medium text-white lg:text-[44px] md:text-4xl pl-2 flex items-center">
               Sobre Nosotros
             </h2>
-            <p className="text-white xl:text-lg max-xl:text-base xl:h-12 max-xl:h-[72px] mb-5 oxygen flex items-center">
+            <p className="text-white xl:text-lg max-xl:text-base xl:h-12 max-xl:h-[72px] lg:mb-5 md:mb-2 oxygen flex items-center">
               IMC es una compañía enfocada en la elaboración de música para
               cine, televisión y videojuegos. ¡Conoce más acerca de nosotros
               aquí!
@@ -30,7 +30,7 @@ const AboutUsSection = () => {
           </div>
 
           {/* Logo */}
-          <div className="xl:w-[550px] max-xl:w-[400px] h-full xl:ml-[110px] max-xl:ml-[80px] px-[15px] py-4">
+          <div className="xl:w-[550px] lg:w-[400px] md:w-[720px] h-full xl:ml-[110px] lg:ml-[80px] px-[15px] py-4">
             <Image src={Logo} alt="logo" className="w-full h-full" />
           </div>
         </div>

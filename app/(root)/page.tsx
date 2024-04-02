e3deebe0f9b15f67b0758941eb69703f771f6a11
Saul Paredes <saulkurosaki13@gmail.com>
2024-04-02 15:51:22 -0600

    feat(Home): implement a better layout design for the navbar spacing

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,7 @@ import HeroBanner from "@/components/HeroBanner";
 
 export default function Home() {
   return (
-    <div className="bg-[#2e2414] w-full h-full">
+    <div className="bg-[#2e2414] w-full h-full max-md:pt-14 md:pt-[70px] xl:pt-14 lg:pt-12">
       <HeroBanner />
 
       <AboutUsSection />

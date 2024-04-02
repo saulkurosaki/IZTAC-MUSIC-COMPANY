179bc6c169b64d0c7cefcff1d4b8017247baaeb3
Saul Paredes <saulkurosaki13@gmail.com>
2024-04-02 16:06:59 -0600

    feat(OurServicesSection): implement the container layout

--- components/OurServicesSection.tsx
+++ components/OurServicesSection.tsx
@@ -1,5 +1,9 @@
 const OurServicesSection = () => {
-  return <div>OurServicesSection component Test c:</div>;
+  return (
+    <section className="w-full h-[775px] bg-[#B99253] py-8">
+      <div className="container w-[1320px] h-full px-[15px]"></div>
+    </section>
+  );
 };
 
 export default OurServicesSection;

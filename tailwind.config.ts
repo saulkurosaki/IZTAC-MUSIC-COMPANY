f730802564223dc4cba1e544d35d0fa17761d47b
Saul Paredes <saulkurosaki13@gmail.com>
2024-04-02 16:54:00 -0600

    feat(tailwind.config.ts): implement a custom type of box shadow

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -82,6 +82,9 @@ const config = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      boxShadow: {
+        "1": "0 4px 16px rgba(0, 0, 0, 0.12)",
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],

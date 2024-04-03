4f63a53b9012b7d1f836be181523e196fb7e0459
Saul Paredes <saulkurosaki13@gmail.com>
2024-04-03 15:38:21 -0600

    feat(PortfolioSection): implement the component top waves

--- components/PortfolioSection.tsx
+++ components/PortfolioSection.tsx
@@ -1,9 +1,39 @@
+import Image from "next/image";
+
+import Port_Top_Waves from "../assets/portfolio_top_waves.svg";
+import Port_Bottom_Waves from "../assets/portfolio_bottom_waves.svg";
+import Portfolio_Sec_Bg from "../assets/portfolio_sec_bg.jpg";
+
 const PortfolioSection = () => {
   return (
-    <section className="w-full h-[953px]">
-      <span className="w-full h-[953px] portfolio-sec-bg bg-cover bg-fixed bg-center absolute overflow-hidden" />
-      <div className="w-full h-[411px] pt-64 pb-10"></div>
-      <div className="w-full h-[542px] py-6"></div>
+    <section className="relative w-full h-[953px]">
+      <span className="absolute inset-0 z-0">
+        <Image
+          src={Portfolio_Sec_Bg}
+          alt="background"
+          className="w-full h-full object-cover bg-cover bg-fixed bg-center"
+        />
+      </span>
+
+      <div className="w-full h-[411px] absolute top-0 left-0 z-10 overflow-hidden">
+        <Image
+          src={Port_Top_Waves}
+          alt="Top Waves"
+          className="w-full h-auto object-contain"
+        />
+      </div>
+      {/* <div className="w-full h-[411px] pt-64 pb-10">
+      </div> */}
+
+      {/* <div className="w-full h-[542px] absolute bottom-0 right-0 z-10 flex items-end overflow-hidden">
+        <Image
+          src={Port_Bottom_Waves}
+          alt="Bottom Waves"
+          className="w-full h-auto object-contain"
+        />
+      </div> */}
+      {/* <div className="w-full h-[542px] py-6">
+      </div> */}
     </section>
   );
 };

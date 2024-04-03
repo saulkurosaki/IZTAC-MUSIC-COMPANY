b9f020e2ff64d4c36a031157167eec6837fd64fb
Saul Paredes <saulkurosaki13@gmail.com>
2024-04-03 12:59:36 -0600

    feat(PortfolioSection): implement the component fixed background image along as the basing layout of the component

--- components/PortfolioSection.tsx
+++ components/PortfolioSection.tsx
@@ -1,5 +1,11 @@
 const PortfolioSection = () => {
-  return <div>PortfolioSection</div>;
+  return (
+    <section className="w-full h-[953px]">
+      <span className="w-full h-[953px] portfolio-sec-bg bg-cover bg-fixed bg-center absolute overflow-hidden" />
+      <div className="w-full h-[411px] pt-64 pb-10"></div>
+      <div className="w-full h-[542px] py-6"></div>
+    </section>
+  );
 };
 
 export default PortfolioSection;

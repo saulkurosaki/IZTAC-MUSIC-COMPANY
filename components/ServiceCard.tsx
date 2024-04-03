192c7df120770f2a5b6c19bd7009c092652bc26b
Saul Paredes <saulkurosaki13@gmail.com>
2024-04-02 19:28:18 -0600

    feat(ServiceCard): implement the complete ServiceCard component

--- components/ServiceCard.tsx
+++ components/ServiceCard.tsx
@@ -1,5 +1,36 @@
-const ServiceCard = () => {
-  return <div>ServiceCard component Test c:</div>;
+import Image, { StaticImageData } from "next/image";
+import Link from "next/link";
+
+type ServiceCardProps = {
+  image: StaticImageData;
+  title: string;
+  description: string;
+};
+
+const ServiceCard = ({ image, title, description }: ServiceCardProps) => {
+  return (
+    <div className="w-full h-full px-[15px] py-4">
+      <div className="flex flex-col w-full h-full overflow-hidden shadow-1 bg-white rounded-[0.4rem]">
+        <Image
+          src={image}
+          alt="our_services_image"
+          className="w-full h-auto object-contain"
+        />
+        <div className="flex flex-col w-full h-[269px] px-4 pt-3 pb-5 justify-around">
+          <h3 className="w-[95%] h-[85px] oxygen text-4xl">{title}</h3>
+          <p className="w-full h-[72px] oxygen ">{description}</p>
+          <div className="w-full h-11">
+            <Link
+              href="/contact-us"
+              className="flex w-32 h-full bg-[#2e2414] justify-center items-center text-white text-lg oxygen rounded-md"
+            >
+              Contáctanos
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ServiceCard;
--- lib/constants.ts
+++ lib/constants.ts
@@ -1,18 +1,22 @@
+import OurServices1 from "../assets/our_services_image_1.webp";
+import OurServices2 from "../assets/our_services_image_2.webp";
+import OurServices3 from "../assets/our_services_image_3.webp";
+
 export const services = {
   coaching: {
-    image: "../assets/our_services_image_1.webp",
+    image: OurServices1,
     title: "Asesoría Artística y Vocal Coaching",
     description:
       "Asesorías personalizadas para artistas, cantantes, productores, compositores y músicos que buscan impulsar su carrera.",
   },
   composition: {
-    image: "../assets/our_services_image_2.webp",
+    image: OurServices2,
     title: "Composición para Media",
     description:
       "Composiciones personalizadas para cine, televisión, comerciales y videojuegos adaptables a presupuesto y objetivos.",
   },
   mastery: {
-    image: "../assets/our_services_image_3.webp",
+    image: OurServices3,
     title: "Mezcla y Masterización",
     description:
       "Ingeniería profesional y eficaz para la mezcla y masterización de proyectos musicales.",

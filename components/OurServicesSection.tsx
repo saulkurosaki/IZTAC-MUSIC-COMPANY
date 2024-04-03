f5e0efdf6fb3b52c6a611aab4553f500f48e5775
Saul Paredes <saulkurosaki13@gmail.com>
2024-04-02 20:13:33 -0600

    feat(OurServicesSection): implement the on medium devices responsive layout

--- components/OurServicesSection.tsx
+++ components/OurServicesSection.tsx
@@ -2,9 +2,9 @@ import ServiceCard from "./ServiceCard";
 import { services } from "@/lib/constants";
 
 const OurServicesSection = () => (
-  <section className="w-full xl:h-[775px] lg:h-[714px] bg-[#B99253] py-8">
-    <div className="container xl:w-[1320px] lg:w-[960px] h-full px-0">
-      <div className="flex w-full h-full items-stretch">
+  <section className="w-full xl:h-[775px] lg:h-[714px] md:h-[2815px] bg-[#B99253] py-8">
+    <div className="container xl:w-[1320px] lg:w-[960px] md:w-[720px] h-full px-0">
+      <div className="flex w-full h-full max-lg:flex-col items-stretch">
         <ServiceCard
           image={services.coaching.image}
           title={services.coaching.title}

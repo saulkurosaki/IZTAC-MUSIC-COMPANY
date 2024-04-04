0b16acdb3f7e75e3cd20c0eb56b6ab3459900784
Saul Paredes <saulkurosaki13@gmail.com>
2024-04-03 19:27:30 -0600

    feat(PortfolioSection): implement the first portfolio card listen to us button

--- components/PortfolioSection.tsx
+++ components/PortfolioSection.tsx
@@ -52,14 +52,14 @@ const PortfolioSection = () => {
                   alt="portfolio_image_1"
                   className="w-full h-auto object-cover pb-3 rounded-lg"
                 />
-                {/* <div className="w-full h-[38px]">
+                <div className="w-full h-[38px]">
                   <Link
                     href="https://soundcloud.com/zackarymc101/infierno-credit-sequence?si=bc5ce5534680455eb8140f74ed6ab6f7&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing"
                     className="w-24 h-full flex justify-center items-center rounded-lg text-[#2e2414] text-lg bg-[#B99253] oxygen"
                   >
                     Escucha
                   </Link>
-                </div> */}
+                </div>
               </div>
 
               {/* <div className="flex flex-col w-auto h-auto px-2">

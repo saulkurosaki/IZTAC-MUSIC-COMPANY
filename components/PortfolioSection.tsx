a6d437276d53d8903446b20a4136b8f271fcd2e4
Saul Paredes <saulkurosaki13@gmail.com>
2024-04-03 19:29:55 -0600

    feat(PortfolioSection): implement the third portfolio card image

--- components/PortfolioSection.tsx
+++ components/PortfolioSection.tsx
@@ -79,11 +79,11 @@ const PortfolioSection = () => {
               </div>
 
               <div className="flex flex-col w-auto h-auto px-2">
-                {/* <Image
+                <Image
                   src={Portfolio_Image_3}
                   alt="portfolio_image_1"
                   className="w-full h-auto object-cover pb-3 rounded-lg"
-                /> */}
+                />
                 {/* <div className="w-full h-[38px]">
                   <Link
                     href="https://soundcloud.com/zackarymc101/frustration?si=b4fa00fffa494e2db8b0d6eff2440bcd&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing"

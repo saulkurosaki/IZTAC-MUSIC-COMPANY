de6af2b4a64fe3168edf1483f7685f62f9c76854
Saul Paredes <saulkurosaki13@gmail.com>
2024-04-03 19:28:58 -0600

    feat(PortfolioSection): implement the second portfolio card listen to us button

--- components/PortfolioSection.tsx
+++ components/PortfolioSection.tsx
@@ -68,14 +68,14 @@ const PortfolioSection = () => {
                   alt="portfolio_image_1"
                   className="w-full h-auto object-cover pb-3 rounded-lg"
                 />
-                {/* <div className="w-full h-[38px]">
+                <div className="w-full h-[38px]">
                   <Link
                     href="https://soundcloud.com/zackarymc101/forseer?si=b97584bca96c42218d01529db20aff77&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing"
                     className="w-24 h-full flex justify-center items-center rounded-lg text-[#2e2414] text-lg bg-[#B99253] oxygen"
                   >
                     Escucha
                   </Link>
-                </div> */}
+                </div>
               </div>
 
               {/* <div className="flex flex-col w-auto h-auto px-2">

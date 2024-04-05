9069e88639e188645c07b4218c77ab19bca83385
Saul Paredes <saulkurosaki13@gmail.com>
2024-04-05 13:20:30 -0600

    feat(ContactUsSection): implement the complete ContactUsSection component

--- components/ContactUsSection.tsx
+++ components/ContactUsSection.tsx
@@ -1,15 +1,39 @@
 import Image from "next/image";
 
 import ContactUs_Bg from "../assets/contactus_sec_img.svg";
+import Link from "next/link";
 
 const ContactUsSection = () => {
   return (
-    <section className="relative w-full h-[379px] bg-[#B99253]">
+    <section className="relative w-full h-[379px] bg-[#B99253] pt-16 pb-52">
       <Image
         src={ContactUs_Bg}
         alt="background"
-        className="w-full h-auto object-cover absolute bottom-0 right-0 z-10 overflow-hidden"
+        className="w-full h-auto object-cover absolute bottom-0 right-0 z-0 overflow-hidden"
       />
+
+      <div className="container w-[1320px] h-full p-0 relative z-10">
+        <div className="flex w-full h-full">
+          <div className="flex w-[990px] h-full px-[15px] pb-7 flex-col justify-around">
+            <h3 className="w-full h-11 text-4xl font-semibold text-gray-900 oxygen">
+              ¿Tienes algún proyecto?
+            </h3>
+            <p className="w-full h-6 text-gray-800 oxygen">
+              Contáctanos para poder apoyarte
+            </p>
+          </div>
+          <div className="w-[330px] h-full px-[15px] pt-2">
+            <div className="w-full h-12 flex justify-end">
+              <Link
+                href="/contact-us"
+                className="w-40 h-full bg-[#2e2414] text-2xl text-white px-4 py-2 rounded-md flex justify-center items-center"
+              >
+                Contáctanos
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };

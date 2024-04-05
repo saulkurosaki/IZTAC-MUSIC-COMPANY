ae225de574e483f36220c299dc9af745f0f68b6e
Saul Paredes <saulkurosaki13@gmail.com>
2024-04-05 13:34:17 -0600

    feat(ContactUsSection): implement the on medium devices responsive layout

--- components/ContactUsSection.tsx
+++ components/ContactUsSection.tsx
@@ -5,14 +5,14 @@ import Link from "next/link";
 
 const ContactUsSection = () => {
   return (
-    <section className="relative w-full h-[379px] bg-[#B99253] pt-16 pb-52">
+    <section className="relative w-full lg:h-[379px] md: bg-[#B99253] pt-16 pb-52">
       <Image
         src={ContactUs_Bg}
         alt="background"
         className="w-full h-auto object-cover absolute bottom-0 right-0 z-0 overflow-hidden"
       />
 
-      <div className="container xl:w-[1320px] lg:w-[960px] h-full p-0 relative z-10">
+      <div className="container xl:w-[1320px] lg:w-[960px] md:w-[720px] h-full p-0 relative z-10">
         <div className="flex w-full h-full">
           <div className="flex w-[990px] h-full px-[15px] pb-7 flex-col justify-around">
             <h3 className="w-full h-11 text-4xl font-semibold text-gray-900 oxygen">

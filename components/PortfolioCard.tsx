e4fcbe31b525b22ae4c6cb5af674c593e3fa9404
Saul Paredes <saulkurosaki13@gmail.com>
2024-04-04 18:35:34 -0600

    feat(PortfolioCard): implement the complete PortfolioCard component

--- components/PortfolioCard.tsx
+++ components/PortfolioCard.tsx
@@ -1,5 +1,27 @@
-const PortfolioCard = () => {
-  return <div>PortfolioCard component Test c:</div>;
+import Image, { StaticImageData } from "next/image";
+import Link from "next/link";
+
+type PortfolioCardProps = {
+  image: StaticImageData;
+  song: string;
 };
+
+const PortfolioCard = ({ image, song }: PortfolioCardProps) => (
+  <div className="flex flex-col w-auto h-auto px-2">
+    <Image
+      src={image}
+      alt="portfolio_image_1"
+      className="w-full h-auto object-cover pb-3 rounded-lg"
+    />
+    <div className="w-full h-[38px]">
+      <Link
+        href={song}
+        className="w-24 h-full flex justify-center items-center rounded-lg text-[#2e2414] text-lg bg-[#B99253] oxygen"
+      >
+        Escucha
+      </Link>
+    </div>
+  </div>
+);
 
 export default PortfolioCard;

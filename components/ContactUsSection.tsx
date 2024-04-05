0aeaad8c06361f00bad17d3943baff62b2deaf84
Saul Paredes <saulkurosaki13@gmail.com>
2024-04-05 13:53:42 -0600

    feat(ContactUsSection): implement the on small and extra small devices responsive layout

--- components/ContactUsSection.tsx
+++ components/ContactUsSection.tsx
@@ -5,16 +5,16 @@ import Link from "next/link";
 
 const ContactUsSection = () => {
   return (
-    <section className="relative w-full lg:h-[379px] md: bg-[#B99253] pt-16 pb-52">
+    <section className="relative w-full lg:h-[379px] md:h-[440px] bg-[#B99253] pt-16 pb-52">
       <Image
         src={ContactUs_Bg}
         alt="background"
         className="w-full h-auto object-cover absolute bottom-0 right-0 z-0 overflow-hidden"
       />
 
-      <div className="container xl:w-[1320px] lg:w-[960px] md:w-[720px] h-full p-0 relative z-10">
-        <div className="flex w-full h-full">
-          <div className="flex w-[990px] h-full px-[15px] pb-7 flex-col justify-around">
+      <div className="container xl:w-[1320px] lg:w-[960px] md:w-[720px] lg:h-full md:h-[110px] max-md:h-56 p-0 relative z-10">
+        <div className="flex w-full h-full max-md:flex-col">
+          <div className="flex md:w-[990px] max-md:w-full md:h-full max-md:h-36 px-[15px] pb-7 flex-col md:justify-around max-md:justify-between">
             <h3 className="w-full h-11 text-4xl font-semibold text-gray-900 oxygen">
               ¿Tienes algún proyecto?
             </h3>
@@ -22,7 +22,7 @@ const ContactUsSection = () => {
               Contáctanos para poder apoyarte
             </p>
           </div>
-          <div className="w-[330px] h-full px-[15px] pt-2">
+          <div className="md:w-[330px] max-md:w-full md:h-full max-md:h-14 px-[15px] pt-2">
             <div className="w-full h-12 flex justify-end">
               <Link
                 href="/contact-us"

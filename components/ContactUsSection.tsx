013b2ed09b73296307a6b17f743d2c108f5cc9f9
Saul Paredes <saulkurosaki13@gmail.com>
2024-04-05 11:25:29 -0600

    feat(ContactUsSection): implement the component svg background

--- components/ContactUsSection.tsx
+++ components/ContactUsSection.tsx
@@ -1,5 +1,17 @@
+import Image from "next/image";
+
+import ContactUs_Bg from "../assets/contactus_sec_img.svg";
+
 const ContactUsSection = () => {
-  return <div>ContactUsSection component Test c:</div>;
+  return (
+    <section className="relative w-full h-[379px] bg-[#B99253]">
+      <Image
+        src={ContactUs_Bg}
+        alt="background"
+        className="w-full h-auto object-cover absolute bottom-0 right-0 z-10 overflow-hidden"
+      />
+    </section>
+  );
 };
 
 export default ContactUsSection;

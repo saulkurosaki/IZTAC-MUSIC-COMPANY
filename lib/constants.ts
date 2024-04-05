91294512b76a2c68791d9f4ab3b5867250f9ca2b
Saul Paredes <saulkurosaki13@gmail.com>
2024-04-04 18:34:39 -0600

    feat(constants.ts): implement the portfolioSongs constants object

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,6 +1,9 @@
 import OurServices1 from "../assets/our_services_image_1.webp";
 import OurServices2 from "../assets/our_services_image_2.webp";
 import OurServices3 from "../assets/our_services_image_3.webp";
+import Portfolio_Image_1 from "../assets/portfolio_image_1.webp";
+import Portfolio_Image_2 from "../assets/portfolio_image_2.webp";
+import Portfolio_Image_3 from "../assets/portfolio_image_3.webp";
 
 export const services = {
   coaching: {
@@ -20,5 +23,20 @@ export const services = {
     title: "Mezcla y Masterización",
     description:
       "Ingeniería profesional y eficaz para la mezcla y masterización de proyectos musicales.",
+  },
+};
+
+export const portfolioSongs = {
+  1: {
+    image: Portfolio_Image_1,
+    song: "https://soundcloud.com/zackarymc101/infierno-credit-sequence?si=bc5ce5534680455eb8140f74ed6ab6f7&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing",
+  },
+  2: {
+    image: Portfolio_Image_2,
+    song: "https://soundcloud.com/zackarymc101/forseer?si=b97584bca96c42218d01529db20aff77&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing",
+  },
+  3: {
+    image: Portfolio_Image_3,
+    song: "https://soundcloud.com/zackarymc101/frustration?si=b4fa00fffa494e2db8b0d6eff2440bcd&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing",
   },
 };

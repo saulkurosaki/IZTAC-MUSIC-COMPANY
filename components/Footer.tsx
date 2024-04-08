959e2acce3f27fe02f87e775ababc20f5c5513ce
Saul Paredes <saulkurosaki13@gmail.com>
2024-04-07 22:01:19 -0600

    feat(Footer): implement the upper section description division soundclound link image

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -10,7 +10,7 @@ const Footer = () => {
     <footer className="w-full h-[438px] bg-[#2e2414]">
       <div className="w-full h-[381px]">
         <div className="w-full h-[138px] pt-8">
-          {/* <div className="container w-[1320px] h-full px-[15px]">
+          <div className="container w-[1320px] h-full px-[15px]">
             <div className="flex w-full h-[54px] justify-center mb-2">
               <Link
                 href="https://soundcloud.com/zackarymc101?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing"
@@ -24,11 +24,11 @@ const Footer = () => {
                 />
               </Link>
             </div>
-            <p className="w-full h-6 mb-3 text-center text-white oxygen text-lg">
+            {/* <p className="w-full h-6 mb-3 text-center text-white oxygen text-lg">
               Blvd. Universitario 399-21, Plaza Palmas II, Oficina 16 •
               Juriquilla QRO 76230 • México
-            </p>
-            <div className="w-full h-5 mb-4 flex justify-center">
+            </p> */}
+            {/* <div className="w-full h-5 mb-4 flex justify-center">
               <div className="w-[269px] h-full flex justify-between">
                 <Image
                   src={Main_Logo}
@@ -43,8 +43,8 @@ const Footer = () => {
                   Iztacmusicofficial@gmail.com
                 </Link>
               </div>
-            </div>
-          </div> */}
+            </div> */}
+          </div>
         </div>
         <div className="w-full h-[243px] py-4"></div>
       </div>

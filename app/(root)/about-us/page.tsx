7d95e2d329a2b380a6eb0c730ae8ea6ee1877da0
Saul Paredes <saulkurosaki13@gmail.com>
2024-04-09 16:07:39 -0600

    feat(AboutUsPage): implement the AboutUsPage component initial layout

---
+++ app/(root)/about-us/page.tsx
@@ -0,0 +1,9 @@
+const AboutUsPage = () => {
+  return (
+    <div className="relative bg-[#2e2414] w-full h-full max-md:pt-14 md:pt-[70px] xl:pt-14 lg:pt-12">
+      About Us Page
+    </div>
+  );
+};
+
+export default AboutUsPage;

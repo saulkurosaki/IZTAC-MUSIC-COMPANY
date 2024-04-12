dd4756af986600565d67599781e87f8caad0a0e5
Saul Paredes <saulkurosaki13@gmail.com>
2024-04-11 19:47:43 -0600

    feat(about-us/Header): implement the complete Header component

--- components/about-us/Header.tsx
+++ components/about-us/Header.tsx
@@ -1,5 +1,27 @@
+import Image from "next/image";
+
+import HeaderBg from "../../assets/banner_image_2.webp";
+
 const Header = () => {
-  return <div>About Us Header component Test c:</div>;
+  return (
+    <section className="relative w-full h-[131px]">
+      <span className="absolute inset-0 z-0">
+        <Image
+          src={HeaderBg}
+          alt="background"
+          className="w-full h-full object-cover bg-cover bg-fixed bg-center"
+        />
+      </span>
+
+      <div className="w-full h-full py-6">
+        <div className="container w-[1320px] h-full px-[15px]">
+          <h1 className="w-full h-full mb-2 text-white text-6xl oxygen z-10 relative flex items-center">
+            Sobre Nosotros
+          </h1>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Header;

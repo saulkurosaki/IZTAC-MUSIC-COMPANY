0dddd3dd8e4ded4c08fa6d285e6e6b3370cbb65a
Saul Paredes <saulkurosaki13@gmail.com>
2024-04-11 20:23:34 -0600

    feat(AboutUsPage): implement the WhoAreWe component

--- app/(root)/about-us/page.tsx
+++ app/(root)/about-us/page.tsx
@@ -1,9 +1,12 @@
 import Header from "@/components/about-us/Header";
+import WhoAreWe from "@/components/about-us/WhoAreWe";
 
 const AboutUsPage = () => {
   return (
     <div className="relative bg-[#2e2414] w-full h-full max-md:pt-14 md:pt-[70px] xl:pt-14 lg:pt-12">
       <Header />
+
+      <WhoAreWe />
     </div>
   );
 };

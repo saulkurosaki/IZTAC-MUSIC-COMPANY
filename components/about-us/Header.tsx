fc19982571fd71ea414f597c1fdd4c63c98b9f0c
Saul Paredes <saulkurosaki13@gmail.com>
2024-04-13 09:17:06 -0600

    fix(Header): fix little z-index layout bug

--- components/about-us/Header.tsx
+++ components/about-us/Header.tsx
@@ -4,7 +4,7 @@ import HeaderBg from "../../assets/banner_image_2.webp";
 
 const Header = () => {
   return (
-    <section className="relative w-full lg:h-[131px] md:h-[114px] max-md:h-[101px]">
+    <section className="relative w-full lg:h-[131px] md:h-[114px] max-md:h-[101px] z-10">
       <span className="absolute inset-0 z-0">
         <Image
           src={HeaderBg}
@@ -15,7 +15,7 @@ const Header = () => {
 
       <div className="w-full h-full py-6">
         <div className="container xl:w-[1320px] lg:w-[960px] md:w-[720px] h-full px-[15px]">
-          <h1 className="w-full h-full mb-2 text-white lg:text-6xl md:text-5xl max-md:text-4xl oxygen z-10 relative flex items-center">
+          <h1 className="w-full h-full mb-2 text-white lg:text-6xl md:text-5xl max-md:text-4xl oxygen z-0 relative flex items-center">
             Sobre Nosotros
           </h1>
         </div>

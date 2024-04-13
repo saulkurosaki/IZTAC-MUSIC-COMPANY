107f2fba0ea9fbc018b08922ca00a48650287c55
Saul Paredes <saulkurosaki13@gmail.com>
2024-04-12 22:05:52 -0600

    feat(WhoAreWe): implement the on large devices responsive layout

--- components/about-us/WhoAreWe.tsx
+++ components/about-us/WhoAreWe.tsx
@@ -4,14 +4,14 @@ import Banner_Img from "../../assets/who_are_we_banner_img.svg";
 
 const WhoAreWe = () => {
   return (
-    <section className="w-full h-[712px] pt-8 pb-32 about-us-sec-img bg-no-repeat bg-bottom bg-cover">
-      <div className="container w-[1320px] h-full px-0">
+    <section className="w-full xl:h-[712px] lg:h-[562px] pt-8 pb-32 about-us-sec-img bg-no-repeat bg-bottom bg-cover">
+      <div className="container xl:w-[1320px] lg:w-[960px] h-full px-0">
         <div className="w-full h-full flex items-center">
           <div className="w-[50%] h-[269px] p-4 flex flex-col justify-between">
             <h2 className="h-[53px] text-white text-5xl oxygen flex items-center px-2">
               Que es IMC
             </h2>
-            <p className="h-[140px] text-white text-lg oxygen">
+            <p className="h-[140px] text-white xl:text-lg max-xl:text-base oxygen">
               Iztac Music Company es una productora musical especializada en la
               composición para media. En su equipo se encuentran compositores
               entrenados por los mejores profesores y en las mejores escuelas
@@ -19,7 +19,7 @@ const WhoAreWe = () => {
               of Music y países como Francia, La India, Estonia y Estados Unidos
             </p>
           </div>
-          <div className="w-[550px] h-full ml-auto p-4">
+          <div className="xl:w-[550px] lg:w-[400px] h-full ml-auto p-4">
             <Image src={Banner_Img} alt="image" width={520} height={520} />
           </div>
         </div>

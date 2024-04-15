92fa7e4be360c493161bd427998770b01edf520e
Saul Paredes <saulkurosaki13@gmail.com>
2024-04-15 17:57:44 -0600

    feat(TeamCard): implement the on medium devices responsive layout

--- components/about-us/TeamCard.tsx
+++ components/about-us/TeamCard.tsx
@@ -8,20 +8,20 @@ type TeamCardProps = {
 
 const TeamCard = ({ memberImage, name, description }: TeamCardProps) => {
   return (
-    <div className="w-[50%] h-[50%] py-6">
-      <div className="flex w-full h-full">
-        <div className="xl:w-[220px] lg:w-[160px] h-full px-[15px] flex items-center">
+    <div className="w-[50%] h-[50%] max-lg:w-full max-lg:h-[25%] py-6">
+      <div className="flex w-full h-full max-lg:flex-col">
+        <div className="xl:w-[220px] lg:w-[160px] max-lg:w-full h-full px-[15px] flex justify-center items-center">
           <Image
             src={memberImage}
             alt="team_member_img"
-            className="xl:w-[190px] xl:h-[190px] lg:w-[130px] lg:h-[130px] rounded-full"
+            className="xl:w-[190px] xl:h-[190px] lg:w-[130px] lg:h-[130px] md:w-[285px] md:h-[285px] rounded-full"
           />
         </div>
-        <div className="xl:w-[440px] lg:w-[320px] h-full px-[15px]">
-          <h4 className="w-full text-white xl:text-3xl lg:text-2xl oxygen mb-6">
+        <div className="xl:w-[440px] lg:w-[320px] h-full px-[15px] max-lg:pt-3">
+          <h4 className="w-full text-white xl:text-3xl lg:text-2xl md:text-3xl oxygen mb-6">
             {name}
           </h4>
-          <p className="w-full text-white xl:text-lg lg:text-base oxygen">
+          <p className="w-full text-white xl:text-lg lg:text-base md:text-lg oxygen">
             {description}
           </p>
         </div>

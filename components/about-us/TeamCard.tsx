cc3fa013ad052df6eea28d030a49ea5a580b9d49
Saul Paredes <saulkurosaki13@gmail.com>
2024-04-15 17:37:39 -0600

    feat(TeamCard): implement the on large devices responsive layout

--- components/about-us/TeamCard.tsx
+++ components/about-us/TeamCard.tsx
@@ -10,16 +10,20 @@ const TeamCard = ({ memberImage, name, description }: TeamCardProps) => {
   return (
     <div className="w-[50%] h-[50%] py-6">
       <div className="flex w-full h-full">
-        <div className="w-[220px] h-full px-[15px] flex items-center">
+        <div className="xl:w-[220px] lg:w-[160px] h-full px-[15px] flex items-center">
           <Image
             src={memberImage}
             alt="team_member_img"
-            className="w-[190px] h-[190px] rounded-full"
+            className="xl:w-[190px] xl:h-[190px] lg:w-[130px] lg:h-[130px] rounded-full"
           />
         </div>
-        <div className="w-[440px] h-full px-[15px]">
-          <h4 className="w-full text-white text-3xl oxygen mb-6">{name}</h4>
-          <p className="w-full text-white text-lg oxygen">{description}</p>
+        <div className="xl:w-[440px] lg:w-[320px] h-full px-[15px]">
+          <h4 className="w-full text-white xl:text-3xl lg:text-2xl oxygen mb-6">
+            {name}
+          </h4>
+          <p className="w-full text-white xl:text-lg lg:text-base oxygen">
+            {description}
+          </p>
         </div>
       </div>
     </div>

e2541c6474706a4fc3f73487030fdd666e62c6a2
Saul Paredes <saulkurosaki13@gmail.com>
2024-04-16 21:57:19 -0600

    feat(OurServicesPage): implement the completed Header component

--- app/(root)/our-services/page.tsx
+++ app/(root)/our-services/page.tsx
@@ -1,7 +1,9 @@
+import Header from "@/components/our-services/Header";
+
 const OurServicesPage = () => {
   return (
     <div className="relative bg-[#2e2414] w-full h-full max-md:pt-14 md:pt-[70px] xl:pt-14 lg:pt-12">
-      OurServicesPage component Test c:
+      <Header />
     </div>
   );
 };

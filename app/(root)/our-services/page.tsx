60d204caa89649d7a66381a5b39adddf4b89e688
Saul Paredes <saulkurosaki13@gmail.com>
2024-04-16 21:53:00 -0600

    style(OurServicesPage): implement the page component initial layout styles

--- app/(root)/our-services/page.tsx
+++ app/(root)/our-services/page.tsx
@@ -1,5 +1,9 @@
 const OurServicesPage = () => {
-  return <div>OurServicesPage component Test c:</div>;
+  return (
+    <div className="relative bg-[#2e2414] w-full h-full max-md:pt-14 md:pt-[70px] xl:pt-14 lg:pt-12">
+      OurServicesPage component Test c:
+    </div>
+  );
 };
 
 export default OurServicesPage;

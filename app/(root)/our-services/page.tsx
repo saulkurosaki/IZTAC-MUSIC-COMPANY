80b4465f12a68713ddec23b100a12d70c7a84709
Saul Paredes <saulkurosaki13@gmail.com>
2024-04-16 22:05:53 -0600

    feat(OurServcesPage): implement the OurServicesSection component into the page component

--- app/(root)/our-services/page.tsx
+++ app/(root)/our-services/page.tsx
@@ -1,9 +1,12 @@
+import OurServicesSection from "@/components/OurServicesSection";
 import Header from "@/components/our-services/Header";
 
 const OurServicesPage = () => {
   return (
     <div className="relative bg-[#2e2414] w-full h-full max-md:pt-14 md:pt-[70px] xl:pt-14 lg:pt-12">
       <Header />
+
+      <OurServicesSection />
     </div>
   );
 };

184e25fd35fe9ea32a837d02854c87923a5f4553
Saul Paredes <saulkurosaki13@gmail.com>
2024-04-18 19:50:13 -0600

    feat(ReferenceCard): implement the complete ReferenceCard component

--- components/our-services/ReferenceCard.tsx
+++ components/our-services/ReferenceCard.tsx
@@ -1,5 +1,42 @@
-const ReferenceCard = () => {
-  return <div>ReferenceCard component Test c:</div>;
+import Image, { StaticImageData } from "next/image";
+
+import QuoteSvg from "../../assets/quote_svg.png";
+
+type ReferenceCardProps = {
+  quote: string;
+  author: string;
+  avatar: StaticImageData;
+};
+
+const ReferenceCard = ({ quote, author, avatar }: ReferenceCardProps) => {
+  return (
+    <div className="w-full h-full py-20">
+      <div className="container w-[1320px] h-full px-[15px]">
+        <div className="w-[645px] h-full mx-auto flex">
+          <div className="w-12 h-12 bg-[#2e2414] rounded-tl-lg rounded-bl-lg p-3">
+            <Image src={QuoteSvg} alt="quote" />
+          </div>
+          <div className="w-[597px] h-full bg-white p-6 flex flex-col justify-between">
+            <p className="w-full h-[90px] oxygen text-black text-2xl italic font-light">
+              {quote}
+            </p>
+            <div className="w-full h-10 flex items-center">
+              <Image
+                src={avatar}
+                alt="reference_img"
+                width={40}
+                height={40}
+                className="rounded-full mr-3"
+              />
+              <p className="w-auto h-auto text-xl text-[#959595] font-semibold italic oxygen">
+                {author}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ReferenceCard;

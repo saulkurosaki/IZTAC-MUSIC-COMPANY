19500792b2816e167b1b45e22ecc8c5d3fa598c8
Saul Paredes <saulkurosaki13@gmail.com>
2024-04-18 19:10:02 -0600

    feat(OurReferences): implement the second CarouselItem quote image

--- components/our-services/OurReferences.tsx
+++ components/our-services/OurReferences.tsx
@@ -87,11 +87,11 @@ const OurReferences = () => {
           <CarouselItem className="pl-0">
             <div className="w-full h-full py-20">
               <div className="container w-[1320px] h-full px-[15px]">
-                {/* <div className="w-[645px] h-full mx-auto flex">
+                <div className="w-[645px] h-full mx-auto flex">
                   <div className="w-12 h-12 bg-[#2e2414] rounded-tl-lg rounded-bl-lg p-3">
                     <Image src={QuoteSvg} alt="quote" />
                   </div>
-                  <div className="w-[597px] h-full bg-white p-6 flex flex-col justify-between">
+                  {/* <div className="w-[597px] h-full bg-white p-6 flex flex-col justify-between">
                     <p className="w-full h-[90px] oxygen text-black text-2xl italic font-light">
                       Con las asesorías en IMC logré expandir mi zona de confort
                       con seguridad y logré conseguir mi papel soñado en mi
@@ -109,8 +109,8 @@ const OurReferences = () => {
                         Victoria - Estelar en Heathers
                       </p>
                     </div>
-                  </div>
-                </div> */}
+                  </div> */}
+                </div>
               </div>
             </div>
           </CarouselItem>

4382fbcb01ab412ea7b5ce1b3683059c5a844881
Saul Paredes <saulkurosaki13@gmail.com>
2024-04-18 19:16:57 -0600

    feat(OurReferences): implement the CarouselTracker component

--- components/our-services/OurReferences.tsx
+++ components/our-services/OurReferences.tsx
@@ -150,7 +150,7 @@ const OurReferences = () => {
         <CarouselNext className="absolute top-1/2 right-6 w-16 h-16 max-md:hidden" />
       </Carousel>
 
-      {/* <CarouselTracker api={api} current={current} top="top-[1275px]" /> */}
+      <CarouselTracker api={api} current={current} top="top-[1275px]" />
     </section>
   );
 };

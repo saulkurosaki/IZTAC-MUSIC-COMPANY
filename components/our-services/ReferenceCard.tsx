814efa96198e5b88d531b006f8c6b6db37402f92
Saul Paredes <saulkurosaki13@gmail.com>
2024-04-19 13:19:39 -0600

    feat(ReferenceCard): implement the on medium devices responsive layout

--- components/our-services/ReferenceCard.tsx
+++ components/our-services/ReferenceCard.tsx
@@ -11,13 +11,13 @@ type ReferenceCardProps = {
 const ReferenceCard = ({ quote, author, avatar }: ReferenceCardProps) => {
   return (
     <div className="w-full h-full py-20">
-      <div className="container xl:w-[1320px] lg:w-[960px] xl:h-full lg:h-[220px] px-[15px]">
-        <div className="xl:w-[645px] lg:w-[465px] h-full mx-auto flex">
+      <div className="container xl:w-[1320px] lg:w-[960px] md:w-[720px] xl:h-full lg:h-[220px] md:h-[280px] px-[15px]">
+        <div className="xl:w-[645px] lg:w-[465px] md:w-[345px] h-full mx-auto flex">
           <div className="w-12 h-12 bg-[#2e2414] rounded-tl-lg rounded-bl-lg xl:p-3 max-xl:p-4">
             <Image src={QuoteSvg} alt="quote" />
           </div>
-          <div className="xl:w-[597px] lg:w-[417px] h-full bg-white p-6 flex flex-col">
-            <p className="w-full h-[90px] oxygen text-black xl:text-2xl lg:text-xl italic font-light xl:mb-4 lg:mb-8">
+          <div className="xl:w-[597px] lg:w-[417px] md:w-[297px] h-full bg-white p-6 flex flex-col">
+            <p className="w-full lg:h-[90px] max-lg:h-[150px] oxygen text-black xl:text-2xl md:text-xl italic font-light xl:mb-4 md:mb-8">
               {quote}
             </p>
             <div className="w-full h-10 flex items-center">
@@ -28,7 +28,7 @@ const ReferenceCard = ({ quote, author, avatar }: ReferenceCardProps) => {
                 height={40}
                 className="rounded-full mr-3"
               />
-              <p className="w-auto h-auto text-xl text-[#959595] font-semibold italic oxygen">
+              <p className="w-auto h-auto lg:text-xl max-lg:text-lg text-[#959595] font-semibold italic oxygen">
                 {author}
               </p>
             </div>

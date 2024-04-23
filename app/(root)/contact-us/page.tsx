d9f1da769742991afcd71634244b61c3fd2c7e15
Saul Paredes <saulkurosaki13@gmail.com>
2024-04-23 13:13:43 -0600

    feat(ContactUsPage): implement the Header component within the page

--- app/(root)/contact-us/page.tsx
+++ app/(root)/contact-us/page.tsx
@@ -1,9 +1,11 @@
+import Header from "@/components/contact-us/Header";
+
 const ContactUsPage = () => {
   return (
     <div className="relative bg-[#2e2414] w-full h-full max-md:pt-14 md:pt-[70px] xl:pt-14 lg:pt-12">
-      Contact Us Page component Test c:
+      <Header />
     </div>
-  )
-}
+  );
+};
 
-export default ContactUsPage
+export default ContactUsPage;

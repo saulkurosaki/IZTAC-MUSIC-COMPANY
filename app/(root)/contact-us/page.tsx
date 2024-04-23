be0fa1b212acefb69c8bba6f6cf6e21cf5029fdb
Saul Paredes <saulkurosaki13@gmail.com>
2024-04-23 13:17:33 -0600

    feat(ContactUsPage): implement the ContactForm component within the page

--- app/(root)/contact-us/page.tsx
+++ app/(root)/contact-us/page.tsx
@@ -1,9 +1,12 @@
+import ContactForm from "@/components/contact-us/ContactForm";
 import Header from "@/components/contact-us/Header";
 
 const ContactUsPage = () => {
   return (
     <div className="relative bg-[#2e2414] w-full h-full max-md:pt-14 md:pt-[70px] xl:pt-14 lg:pt-12">
       <Header />
+
+      <ContactForm />
     </div>
   );
 };

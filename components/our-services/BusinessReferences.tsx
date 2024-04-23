159f0d4e24f76dbbaf52d74616fbf9b33cf586c3
Saul Paredes <saulkurosaki13@gmail.com>
2024-04-23 12:04:40 -0600

    feat(BusinessReferences): implement the upper section division layout

--- components/our-services/BusinessReferences.tsx
+++ components/our-services/BusinessReferences.tsx
@@ -10,16 +10,16 @@ import Reference_6 from "../../assets/business_references_img_6.svg";
 const BusinessReferences = () => {
   return (
     <section className="w-full h-[380px] bg-white pt-12 pb-8">
-      {/* <div className="container w-[1320px] h-full px-[15px] flex flex-col">
+      <div className="container w-[1320px] h-full px-[15px] flex flex-col">
         <div className="w-full h-[150px] flex flex-col">
-          <h2 className="w-full h-[70px] text-[42px] oxygen flex items-center justify-center">
+          {/* <h2 className="w-full h-[70px] text-[42px] oxygen flex items-center justify-center">
             Nuestras Referencias
-          </h2>
-          <p className="w-full h-[30px] text-lg oxygen text-[#7a7a7a] flex items-center justify-center">
+          </h2> */}
+          {/* <p className="w-full h-[30px] text-lg oxygen text-[#7a7a7a] flex items-center justify-center">
             Algunas de las empresas con las que hemos colaborado.
-          </p>
+          </p> */}
         </div>
-        <div className="w-full h-[150px] flex pb-12">
+        {/* <div className="w-full h-[150px] flex pb-12">
           <Image
             src={Reference_1}
             alt="reference_image"
@@ -56,8 +56,8 @@ const BusinessReferences = () => {
             width={215}
             className="object-contain"
           />
-        </div>
-      </div> */}
+        </div> */}
+      </div>
     </section>
   );
 };

30ad7d0e049bce144361cc6a74ba5e561f6739be
Saul Paredes <saulkurosaki13@gmail.com>
2024-04-23 13:11:10 -0600

    style(ContactUsPage): implement the ContactUsPage layout styles

--- app/(root)/contact-us/page.tsx
+++ app/(root)/contact-us/page.tsx
@@ -1,5 +1,9 @@
 const ContactUsPage = () => {
-  return <div>Contact Us Page component Test c:</div>;
-};
+  return (
+    <div className="relative bg-[#2e2414] w-full h-full max-md:pt-14 md:pt-[70px] xl:pt-14 lg:pt-12">
+      Contact Us Page component Test c:
+    </div>
+  )
+}
 
-export default ContactUsPage;
+export default ContactUsPage

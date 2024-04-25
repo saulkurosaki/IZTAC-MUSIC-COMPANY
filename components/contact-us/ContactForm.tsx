80f85b10d7d596c51c4a59307a5e32aa1b34de22
Saul Paredes <saulkurosaki13@gmail.com>
2024-04-25 17:34:39 -0600

    feat(ContactForm): implement the fourth shadcn FormField component layout and props

--- components/contact-us/ContactForm.tsx
+++ components/contact-us/ContactForm.tsx
@@ -115,13 +115,13 @@ const ContactForm = () => {
                       </FormItem>
                     )}
                   />
-                  {/* <FormField
+                  <FormField
                     control={form.control}
                     name="company"
                     render={({ field }) => (
                       <FormItem className="w-full flex-grow flex items-center py-2">
                         <FormControl>
-                          <div className="w-full h-full flex">
+                          {/* <div className="w-full h-full flex">
                             <p className="w-[185px] h-full flex items-center text-lg text-[#727272] oxygen">
                               Empresa
                             </p>
@@ -129,11 +129,11 @@ const ContactForm = () => {
                               {...field}
                               className="w-[665px] h-full border-[#3d3d3d36]"
                             />
-                          </div>
+                          </div> */}
                         </FormControl>
                       </FormItem>
                     )}
-                  /> */}
+                  />
                   {/* <FormField
                     control={form.control}
                     name="matter"

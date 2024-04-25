9cc160b2ccd90df3954dc0cd8d35bfdbae7e7934
Saul Paredes <saulkurosaki13@gmail.com>
2024-04-25 17:27:54 -0600

    feat(ContactForm): implement the first shadcn FormField component layout and props

--- components/contact-us/ContactForm.tsx
+++ components/contact-us/ContactForm.tsx
@@ -52,13 +52,13 @@ const ContactForm = () => {
                   onSubmit={form.handleSubmit(onSubmit)}
                   className="w-full h-full flex flex-col"
                 >
-                  {/* <FormField
+                  <FormField
                     control={form.control}
                     name="name"
                     render={({ field }) => (
                       <FormItem className="w-full flex-grow flex items-center py-2">
                         <FormControl>
-                          <div className="w-full h-full flex">
+                          {/* <div className="w-full h-full flex">
                             <p className="w-[185px] h-full flex items-center text-lg text-[#727272] oxygen">
                               Nombre
                               <span className="text-sm text-[#ff4141]">
@@ -69,11 +69,11 @@ const ContactForm = () => {
                               {...field}
                               className="w-[665px] h-full border-[#3d3d3d36]"
                             />
-                          </div>
+                          </div> */}
                         </FormControl>
                       </FormItem>
                     )}
-                  /> */}
+                  />
                   {/* <FormField
                     control={form.control}
                     name="number"

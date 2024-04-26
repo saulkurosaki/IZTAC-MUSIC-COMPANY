12b6aa011f692fd094b75e1516ac7186d9599b7c
Saul Paredes <saulkurosaki13@gmail.com>
2024-04-25 21:14:29 -0600

    feat(ContactSection): implement the on small and extra small devices responsive layout

--- components/contact-us/ContactSection.tsx
+++ components/contact-us/ContactSection.tsx
@@ -5,25 +5,25 @@ import ContactForm from "./ContactForm";
 
 const ContactSection = () => {
   return (
-    <section className="w-full xl:h-[546px] lg:h-[570px] max-lg:h-[690px] bg-white py-10">
-      <div className="container xl:w-[1320px] lg:w-[960px] max-lg:w-[720px] h-full px-0">
+    <section className="w-full xl:h-[546px] lg:h-[570px] md:h-[690px] max-md:h-[1030px] bg-white py-10">
+      <div className="container xl:w-[1320px] lg:w-[960px] md:w-[720px] max-md:w-[320px] md:h-full max-md:h-[958px] px-0">
         <div className="w-full h-full flex max-lg:flex-col">
-          <div className="xl:w-[880px] lg:w-[640px] max-lg:w-full lg:h-full max-lg:h-[466px] max-lg:mt-6 px-[15px]">
-            <p className="w-full xl:h-12 lg:h-[72px] max-lg:h-[48px] mb-4 lg:text-lg max-lg:text-base text-[#727272] flex items-center oxygen">
+          <div className="xl:w-[880px] lg:w-[640px] max-lg:w-full lg:h-full md:h-[466px] max-md:h-[766px] max-lg:mt-6 px-[15px]">
+            <p className="w-full xl:h-12 lg:h-[72px] md:h-[48px] max-md:h-[120px] mb-4 lg:text-lg md:text-base max-md:text-lg text-[#727272] flex items-center oxygen">
               Mándenos mensaje sobre cualquier cosa relacionada con nuestra
               empresa o servicios. <br />
               Haremos todo lo posible para contactarle lo antes posible.
             </p>
-            <div className="w-full h-[402px]">
+            <div className="w-full md:h-[402px] max-md:h-[630px]">
               <ContactForm />
             </div>
           </div>
-          <div className="xl:w-[440px] lg:w-[320px] max-lg:w-[720px] lg:h-full max-lg:h-[96px] max-lg:mt-6 px-[15px]">
-            <div className="xl:w-[410px] lg:w-[290px] max-lg:w-[690px] xl:h-24 lg:h-36 max-lg:h-24 pl-2">
+          <div className="xl:w-[440px] lg:w-[320px] md:w-[720px] max-md:w-[320px] lg:h-full md:h-[96px] max-md:h-[144px] max-lg:mt-6 px-[15px]">
+            <div className="xl:w-[410px] lg:w-[290px] md:w-[690px] max-md:w-[290px] xl:h-24 lg:h-36 max-lg:h-full pl-2">
               <p className="w-full h-6 text-[#6C757D] oxygen">
                 IMC (Iztac Music Company)
               </p>
-              <p className="w-full xl:h-6 lg:h-12 max-lg:h-6 flex text-[#6C757D] oxygen">
+              <p className="w-full xl:h-6 lg:h-12 md:h-6 max-md:h-12 flex text-[#6C757D] oxygen">
                 <Image
                   src={LocationLogo}
                   alt="location_logo"
@@ -31,7 +31,7 @@ const ContactSection = () => {
                 />
                 <span>Blvd. Universitario 399-21, Plaza Palmas II</span>
               </p>
-              <p className="w-full xl:h-6 lg:h-12 max-lg:h-6 text-[#6C757D] oxygen">
+              <p className="w-full xl:h-6 lg:h-12 md:h-6 max-md:h-12 text-[#6C757D] oxygen">
                 Oficina 16 • Juriquilla QRO 76230 • México
               </p>
               <p className="w-full h-6 flex text-[#6C757D] oxygen">

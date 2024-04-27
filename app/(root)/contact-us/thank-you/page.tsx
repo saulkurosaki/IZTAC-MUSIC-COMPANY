b17dfc5e67b180e9b12d449ec2cf1b568978b224
Saul Paredes <saulkurosaki13@gmail.com>
2024-04-27 12:55:46 -0600

    style(ContactThankYouPage): implement the page component layout styles

--- app/(root)/contact-us/thank-you/page.tsx
+++ app/(root)/contact-us/thank-you/page.tsx
@@ -1,5 +1,9 @@
 const ContactThankYouPage = () => {
-  return <div>Esta es la ContactThankYouPage component Test c:</div>;
+  return (
+    <div className="relative bg-[#2e2414] w-full h-full max-md:pt-14 md:pt-[70px] xl:pt-14 lg:pt-12">
+      Esta es la ContactThankYouPage component Test c:
+    </div>
+  );
 };
 
 export default ContactThankYouPage;

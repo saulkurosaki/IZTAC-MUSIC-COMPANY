70eafc572789decc418e9e0617d86fdf41e18f62
Saul Paredes <saulkurosaki13@gmail.com>
2024-04-28 02:35:08 -0600

    feat(ContactThankYou): implement the Footer component within the page component

--- app/(root)/contact-us/thank-you/page.tsx
+++ app/(root)/contact-us/thank-you/page.tsx
@@ -1,9 +1,12 @@
+import Footer from "@/components/Footer";
 import Main from "@/components/contact-us/thank-you/Main";
 
 const ContactThankYouPage = () => {
   return (
     <div className="relative bg-[#2e2414] w-full h-full max-md:pt-14 md:pt-[70px] xl:pt-14 lg:pt-12">
       <Main />
+
+      <Footer />
     </div>
   );
 };

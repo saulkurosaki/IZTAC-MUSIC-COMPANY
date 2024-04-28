6c09d2eabde10ec701dfd2961dbbd8d36e3fb647
Saul Paredes <saulkurosaki13@gmail.com>
2024-04-28 11:17:44 -0600

    fix(BlogPage): fix little layout bug

--- app/(root)/blog/page.tsx
+++ app/(root)/blog/page.tsx
@@ -2,7 +2,7 @@ import Header from "@/components/blog/Header";
 
 const BlogPage = () => {
   return (
-    <div className="relative bg-[#2e2414] w-full h-full max-md:pt-14 md:pt-[70px] xl:pt-14 lg:pt-12">
+    <div className="relative bg-[#2e2414] w-full h-full max-md:pt-14 md:pt-[70px] xl:pt-14 lg:pt-[57px]">
       <Header />
     </div>
   );

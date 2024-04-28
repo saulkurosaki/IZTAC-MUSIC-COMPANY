5882489c173615ba34421a30edd3d2d6d014de17
Saul Paredes <saulkurosaki13@gmail.com>
2024-04-28 12:27:00 -0600

    feat(blog/Navbar): implement the navbar first text

--- components/blog/Navbar.tsx
+++ components/blog/Navbar.tsx
@@ -2,9 +2,9 @@ const Navbar = () => {
   return (
     <nav className="w-full h-14 mt-4 px-4 py-2 bg-[#B99253]">
       <div className="container w-[1320px] h-full px-[15px] flex">
-        {/* <div className="w-[66px] h-full flex items-center text-lg text-[#5050507e] oxygen">
+        <div className="w-[66px] h-full flex items-center text-lg text-[#5050507e] oxygen">
           Logros:
-        </div> */}
+        </div>
         {/* <div className="w-[66px] h-full flex items-center justify-center text-lg text-[#222222] font-bold oxygen">
           Todos
         </div> */}

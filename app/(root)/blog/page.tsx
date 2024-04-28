6346242afae53f98efe14a633c10777761806dc5
Saul Paredes <saulkurosaki13@gmail.com>
2024-04-28 12:17:40 -0600

    style(BlogPage): change the background color of the entire BlogPage

--- app/(root)/blog/page.tsx
+++ app/(root)/blog/page.tsx
@@ -3,7 +3,7 @@ import Navbar from "@/components/blog/Navbar";
 
 const BlogPage = () => {
   return (
-    <div className="relative bg-[#2e2414] w-full h-full max-md:pt-14 md:pt-[70px] xl:pt-14 lg:pt-[57px]">
+    <div className="relative bg-[#fff] w-full h-full max-md:pt-14 md:pt-[70px] xl:pt-14 lg:pt-[57px]">
       <Header />
 
       <Navbar />

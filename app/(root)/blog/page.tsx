7b8f375c5d848b399ea7b0f06d4f3f9ce4a190eb
Saul Paredes <saulkurosaki13@gmail.com>
2024-04-28 10:18:31 -0600

    feat(BlogPage): implement the Header component within the page

--- app/(root)/blog/page.tsx
+++ app/(root)/blog/page.tsx
@@ -1,7 +1,9 @@
+import Header from "@/components/blog/Header";
+
 const BlogPage = () => {
   return (
     <div className="relative bg-[#2e2414] w-full h-full max-md:pt-14 md:pt-[70px] xl:pt-14 lg:pt-12">
-      BlogPage component Test c:
+      <Header />
     </div>
   );
 };

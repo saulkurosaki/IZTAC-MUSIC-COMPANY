8355d7d6b66fa7ccd65130206fe28464a1a6ff4c
Saul Paredes <saulkurosaki13@gmail.com>
2024-04-28 11:30:36 -0600

    feat(BlogPage): implement the Navbar component within the page

--- app/(root)/blog/page.tsx
+++ app/(root)/blog/page.tsx
@@ -1,9 +1,12 @@
 import Header from "@/components/blog/Header";
+import Navbar from "@/components/blog/Navbar";
 
 const BlogPage = () => {
   return (
     <div className="relative bg-[#2e2414] w-full h-full max-md:pt-14 md:pt-[70px] xl:pt-14 lg:pt-[57px]">
       <Header />
+
+      <Navbar />
     </div>
   );
 };

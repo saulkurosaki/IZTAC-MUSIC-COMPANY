4758da43005d00a53074691f66a24b8e7863205c
Saul Paredes <saulkurosaki13@gmail.com>
2024-04-28 03:05:15 -0600

    style(BlogPage): implement the BlogPage component initial styles

--- app/(root)/blog/page.tsx
+++ app/(root)/blog/page.tsx
@@ -1,5 +1,9 @@
 const BlogPage = () => {
-  return <div>BlogPage component Test c:</div>;
+  return (
+    <div className="relative bg-[#2e2414] w-full h-full max-md:pt-14 md:pt-[70px] xl:pt-14 lg:pt-12">
+      BlogPage component Test c:
+    </div>
+  );
 };
 
 export default BlogPage;

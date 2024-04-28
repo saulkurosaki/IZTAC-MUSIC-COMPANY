e04b2d5448ebc09f1a049136db6b04a9fb8da8c7
Saul Paredes <saulkurosaki13@gmail.com>
2024-04-28 12:28:29 -0600

    feat(blog/Navbar): implement the navbar fourth text

--- components/blog/Navbar.tsx
+++ components/blog/Navbar.tsx
@@ -11,9 +11,9 @@ const Navbar = () => {
         <div className="w-[162px] h-full flex items-center justify-center text-lg text-[#1e1e1e81] font-bold oxygen">
           Reconocimientos
         </div>
-        {/* <div className="w-[159px] h-full flex items-center justify-center text-lg text-[#1e1e1e81] font-bold oxygen">
+        <div className="w-[159px] h-full flex items-center justify-center text-lg text-[#1e1e1e81] font-bold oxygen">
           Metas cumplidas
-        </div> */}
+        </div>
       </div>
     </nav>
   );

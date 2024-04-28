8778de402fa9ab6d44a5418a7563d1ba231e657a
Saul Paredes <saulkurosaki13@gmail.com>
2024-04-28 12:25:38 -0600

    feat(blog/Navbar): implement the component container layout styles

--- components/blog/Navbar.tsx
+++ components/blog/Navbar.tsx
@@ -1,5 +1,22 @@
 const Navbar = () => {
-  return <div>Blog Navbar component Test c:</div>;
+  return (
+    <nav className="w-full h-14 mt-4 px-4 py-2 bg-[#B99253]">
+      <div className="container w-[1320px] h-full px-[15px] flex">
+        {/* <div className="w-[66px] h-full flex items-center text-lg text-[#5050507e] oxygen">
+          Logros:
+        </div> */}
+        {/* <div className="w-[66px] h-full flex items-center justify-center text-lg text-[#222222] font-bold oxygen">
+          Todos
+        </div> */}
+        {/* <div className="w-[162px] h-full flex items-center justify-center text-lg text-[#1e1e1e81] font-bold oxygen">
+          Reconocimientos
+        </div> */}
+        {/* <div className="w-[159px] h-full flex items-center justify-center text-lg text-[#1e1e1e81] font-bold oxygen">
+          Metas cumplidas
+        </div> */}
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;

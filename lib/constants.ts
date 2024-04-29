67a03196cda27524b8056a55cdfc39e7977eb6e9
Saul Paredes <saulkurosaki13@gmail.com>
2024-04-29 13:03:16 -0600

    fix(constants.ts): change allBlogPosts achievement property for development purposes

--- lib/constants.ts
+++ lib/constants.ts
@@ -107,7 +107,7 @@ export const allBlogPosts = [
       "¡IMC se vuelve internacional! IMC se alía con JMI para el programa Ethno, un taller en donde artistas de 18 a 30 años de diversos países se juntan para aprender de canciones folclóricas de alrededor d...",
     date: "12 feb. 2024",
     image: BlogPostImage1,
-    isAchievement: true,
+    achievement: "goals",
     url: "/blog/metas-cumplidas/imc-manda-a-sus-artistas-a-nivel-internacional",
   },
   {
@@ -117,7 +117,7 @@ export const allBlogPosts = [
       "IMC participa a nivel internacional El fin de semana del 28 de Enero IMC participó en la Global Game Jam, un reto a nivel global donde en 48 horas se les pide a varios equipos elaborar un videojuego c...",
     date: "12 feb. 2024",
     image: BlogPostImage2,
-    isAchievement: false,
+    achievement: "recognitions",
     url: "/blog/reconocimientos/imc-en-la-global-game-jam",
   },
   {
@@ -127,7 +127,7 @@ export const allBlogPosts = [
       '¡Una canción muy queretana! El día 29 de noviembre de 2022 Nuestra liricista Giselle Maldonado ganó un premio estatal por parte de la competencia de SEJUVE Querétaro " Canta a la queretana" por su com...',
     date: "12 feb. 2024",
     image: BlogPostImage3,
-    isAchievement: false,
+    achievement: "recognitions",
     url: "/blog/reconocimientos/nuestra-liricista-gana-premio-estatal",
   },
   {
@@ -137,7 +137,7 @@ export const allBlogPosts = [
       "Isaac Maldonado (Nuestro compositor) Reconocido en Berklee El pasado 28 de Agosto de 2023, Isaac Maldonado apareció en un artículo en la página oficial de instagram de Berklee College of Music en dond...",
     date: "12 feb. 2024",
     image: BlogPostImage4,
-    isAchievement: false,
+    achievement: "recognitions",
     url: "/blog/reconocimientos/nuestro-compositor-es-reconocido-en-berklee",
   },
   {
@@ -147,7 +147,7 @@ export const allBlogPosts = [
       'Una Obra Digna De Laurel La colaboración de IMC con el equipo del director queretano Didier Richards para su corto "Infierno" Dio frutos al ser presentado en diversos festivales a nivel internacional ...',
     date: "12 feb. 2024",
     image: BlogPostImage5,
-    isAchievement: false,
+    achievement: "recognitions",
     url: "/blog/reconocimientos/premio-internacional-a-mejor-soundtrack",
   },
   {
@@ -158,7 +158,7 @@ export const allBlogPosts = [
       "¡Felicidades! María José pudo obtener una beca para entrar a la preparatoria de su preferencia después de tomar asesorías en IMC. Sus habilidades vocales le dieron acceso a una beca artística como voc...",
     date: "12 feb. 2024",
     image: BlogPostImage4,
-    isAchievement: true,
+    achievement: "goals",
     url: "/blog/metas-cumplidas/maria-jose-obtiene-beca-al-quedar-en-el-ensamble-de-su-preparatoria",
   },
   {
@@ -168,7 +168,7 @@ export const allBlogPosts = [
       '¡Que Bello! Victoria, una de nuestras artistas tomando asesorías en IMC logro obtener lo que ella describe como su "Papel Soñado" una vez que se abrieron las audiciones para la adaptación al español d...',
     date: "12 feb. 2024",
     image: BlogPostImage7,
-    isAchievement: true,
+    achievement: "goals",
     url: "/blog/metas-cumplidas/victoria-obtiene-papel-estelar-en-heathers",
   },
   {
@@ -178,7 +178,7 @@ export const allBlogPosts = [
       "¡Felicidades! Después de meses de preparación nuestro alumno Víctor que empezó con sus asesorías en IMC logró pasar por el proceso de audición para conseguir un papel Estelar en la adaptación al españ...",
     date: "12 feb. 2024",
     image: BlogPostImage4,
-    isAchievement: true,
+    achievement: "goals",
     url: "/blog/metas-cumplidas/victor-consigue-papel-estelar-para-la-obra-in-the-heights",
   },
 ];

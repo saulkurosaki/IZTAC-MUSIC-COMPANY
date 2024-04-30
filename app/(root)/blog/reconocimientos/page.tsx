fe68e635e9858e6291d26e7a83bbd6dd6baa7353
Saul Paredes <saulkurosaki13@gmail.com>
2024-04-29 20:46:25 -0600

    feat(RecognitionsPage): implement the Navbar component within the page

--- app/(root)/blog/reconocimientos/page.tsx
+++ app/(root)/blog/reconocimientos/page.tsx
@@ -1,9 +1,12 @@
+import Navbar from "@/components/blog/Navbar";
 import Header from "@/components/blog/recognitions/Header";
 
 const RecognitionsPage = () => {
   return (
     <div className="relative bg-[#fff] w-full h-full max-md:pt-14 md:pt-[70px] xl:pt-14 lg:pt-14">
       <Header />
+
+      <Navbar />
     </div>
   );
 };

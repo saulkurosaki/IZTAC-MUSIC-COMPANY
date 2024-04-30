13f40cfe3e1d18ac1dd5de782789dbc524a019ec
Saul Paredes <saulkurosaki13@gmail.com>
2024-04-29 21:25:37 -0600

    feat(AchievementsPage): implement the AchievementsPage container layout styles

--- app/(root)/blog/metas-cumplidas/page.tsx
+++ app/(root)/blog/metas-cumplidas/page.tsx
@@ -1,5 +1,9 @@
 const AchievementsPage = () => {
-  return <div>AchievementsPage component Test c:</div>;
+  return (
+    <div className="relative bg-[#fff] w-full h-full max-md:pt-14 md:pt-[70px] xl:pt-14 lg:pt-14">
+      AchievementsPage component Test c:
+    </div>
+  );
 };
 
 export default AchievementsPage;

a3bb9dbfdc999ea74f2ce4808480eac2424708c7
Saul Paredes <saulkurosaki13@gmail.com>
2024-04-29 20:14:05 -0600

    style(RecognitionsPage): implement the RecognitionsPage container layout styles

--- app/(root)/blog/reconocimientos/page.tsx
+++ app/(root)/blog/reconocimientos/page.tsx
@@ -1,5 +1,9 @@
 const RecognitionsPage = () => {
-  return <div>RecognitionsPage component Test c:</div>;
+  return (
+    <div className="relative bg-[#fff] w-full h-full max-md:pt-14 md:pt-[70px] xl:pt-14 lg:pt-14">
+      RecognitionsPage component Test c:
+    </div>
+  );
 };
 
 export default RecognitionsPage;

26d555bcef830e0b7dc6b2e65cb45541840fa008
Saul Paredes <saulkurosaki13@gmail.com>
2024-04-29 21:07:27 -0600

    feat(RecognitionsPage): implement the Navbar category property

--- app/(root)/blog/reconocimientos/page.tsx
+++ app/(root)/blog/reconocimientos/page.tsx
@@ -6,7 +6,7 @@ const RecognitionsPage = () => {
     <div className="relative bg-[#fff] w-full h-full max-md:pt-14 md:pt-[70px] xl:pt-14 lg:pt-14">
       <Header />
 
-      <Navbar />
+      <Navbar category="recognitions" />
     </div>
   );
 };

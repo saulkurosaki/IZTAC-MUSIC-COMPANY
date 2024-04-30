75aeecb25ac3baf1236f97af3b78d79314ee2eab
Saul Paredes <saulkurosaki13@gmail.com>
2024-04-29 21:27:27 -0600

    feat(AchievementsPage): implement the Header component within the page

--- app/(root)/blog/metas-cumplidas/page.tsx
+++ app/(root)/blog/metas-cumplidas/page.tsx
@@ -1,7 +1,9 @@
+import Header from "@/components/blog/achievements/Header";
+
 const AchievementsPage = () => {
   return (
     <div className="relative bg-[#fff] w-full h-full max-md:pt-14 md:pt-[70px] xl:pt-14 lg:pt-14">
-      AchievementsPage component Test c:
+      <Header />
     </div>
   );
 };

9854d4d777e5e2925012e89c86e8b4a66f441403
Saul Paredes <saulkurosaki13@gmail.com>
2024-04-29 21:51:30 -0600

    feat(AchievementsPage): implement the Navbar component within the page

--- app/(root)/blog/metas-cumplidas/page.tsx
+++ app/(root)/blog/metas-cumplidas/page.tsx
@@ -1,9 +1,12 @@
+import Navbar from "@/components/blog/Navbar";
 import Header from "@/components/blog/achievements/Header";
 
 const AchievementsPage = () => {
   return (
     <div className="relative bg-[#fff] w-full h-full max-md:pt-14 md:pt-[70px] xl:pt-14 lg:pt-14">
       <Header />
+
+      <Navbar category="achievements" />
     </div>
   );
 };

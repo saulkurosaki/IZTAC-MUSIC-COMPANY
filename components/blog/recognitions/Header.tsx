41f9e292ac90b05281523e0bb8750842c113183e
Saul Paredes <saulkurosaki13@gmail.com>
2024-04-29 20:35:20 -0600

    feat(reconocimientos/Header): implement the Header component container layout styles

--- components/blog/recognitions/Header.tsx
+++ components/blog/recognitions/Header.tsx
@@ -1,5 +1,38 @@
+// import Image from "next/image";
+
+// import HeaderBg from "@/assets/recognitions_bg_img.png";
+
 const Header = () => {
-  return <div>Header component Test c:</div>;
+  return (
+    <section className="relative w-full h-[215px]">
+      {/* <span className="relative block w-full h-full">
+        <Image
+          src={HeaderBg}
+          alt="background"
+          className="w-full h-full object-cover bg-cover bg-fixed bg-center z-0"
+        />
+        <div
+          className="absolute inset-0 z-10"
+          style={{
+            backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.4))`,
+          }}
+        />
+      </span>
+
+      <div className="absolute inset-0 py-12 z-20">
+        <div className="container w-[1320px] h-full px-[15px]">
+          <div className="w-full text-[#eee] hover:opacity-70 font-light oxygen">
+            <h1 className="lg:text-6xl md:text-5xl max-md:text-4xl">
+              Reconocimientos
+            </h1>
+            <h5 className="text-xl">
+              Las menciones que le dan prestigio a la compañía
+            </h5>
+          </div>
+        </div>
+      </div> */}
+    </section>
+  );
 };
 
 export default Header;

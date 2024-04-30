5e7e28761f170961ebc3b0a0aa845bb2459d3c95
Saul Paredes <saulkurosaki13@gmail.com>
2024-04-29 21:07:01 -0600

    feat(BlogPage): implement the Navbar category property

--- app/(root)/blog/page.tsx
+++ app/(root)/blog/page.tsx
@@ -8,7 +8,7 @@ const BlogPage = () => {
     <div className="relative bg-[#fff] w-full h-full max-md:pt-14 md:pt-[70px] xl:pt-14 lg:pt-14">
       <Header />
 
-      <Navbar />
+      <Navbar category="all" />
 
       <BlogPosts />
 

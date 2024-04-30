70de6f6140b12a9cf366a32e89b7f7b2b8282580
Saul Paredes <saulkurosaki13@gmail.com>
2024-04-29 20:16:49 -0600

    style(RecognitionsPage): implement the Header component within the page

--- app/(root)/blog/reconocimientos/page.tsx
+++ app/(root)/blog/reconocimientos/page.tsx
@@ -1,7 +1,9 @@
+import Header from "@/components/blog/recognitions/Header";
+
 const RecognitionsPage = () => {
   return (
     <div className="relative bg-[#fff] w-full h-full max-md:pt-14 md:pt-[70px] xl:pt-14 lg:pt-14">
-      RecognitionsPage component Test c:
+      <Header />
     </div>
   );
 };

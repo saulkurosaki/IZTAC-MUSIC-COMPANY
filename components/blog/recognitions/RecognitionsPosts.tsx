454881ff535f2f452abd44f8fd3d49002778a774
Saul Paredes <saulkurosaki13@gmail.com>
2024-04-29 21:20:26 -0600

    feat(RecognitionsPosts): implement the complete RecognitionsPosts component

--- components/blog/recognitions/RecognitionsPosts.tsx
+++ components/blog/recognitions/RecognitionsPosts.tsx
@@ -1,5 +1,29 @@
+import { allBlogPosts } from "@/lib/constants";
+import BlogCard from "../BlogCard";
+
 const RecognitionsPosts = () => {
-  return <div>RecognitionsPosts component Test c:</div>;
+  return (
+    <section className="w-full h-auto">
+      <div className="container xl:w-[1320px] lg:w-[960px] md:w-[720px] max-md:w-full h-full px-0 py-6">
+        <div className="w-full h-full flex flex-wrap">
+          {allBlogPosts
+            .filter((post) => post.achievement === "recognitions")
+            .map((post, index) => (
+              <BlogCard
+                key={`card_${index + 1}`}
+                author={post.author}
+                title={post.title}
+                description={post.description}
+                date={post.date}
+                image={post.image}
+                achievement={post.achievement}
+                url={post.url}
+              />
+            ))}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default RecognitionsPosts;

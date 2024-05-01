bf1de91c74200392e54b8180e3da2bcdab1ceb3d
Saul Paredes <saulkurosaki13@gmail.com>
2024-05-01 17:41:20 -0600

    feat(AchievementsDetailsPage2): implement the DetailsHeader complete component within the page

--- app/(root)/blog/metas-cumplidas/maria-jose-obtiene-beca-al-quedar-en-el-ensamble-de-su-preparatoria/page.tsx
+++ app/(root)/blog/metas-cumplidas/maria-jose-obtiene-beca-al-quedar-en-el-ensamble-de-su-preparatoria/page.tsx
@@ -1,7 +1,14 @@
+import DetailsHeader from "@/components/blog/blogDetailsPage/DetailsHeader";
+import { achievementsInfo } from "@/lib/constants";
+
 const AchievementsDetailsPage2 = () => {
   return (
     <div className="relative bg-[#fff] w-full h-full max-md:pt-14 md:pt-[70px] xl:pt-14 lg:pt-14">
-      AchievementsDetailsPage2 component Test c:
+      <DetailsHeader
+        title={achievementsInfo[1].title}
+        date={achievementsInfo[1].date}
+        image={achievementsInfo[1].image}
+      />
     </div>
   );
 };

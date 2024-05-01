4b1351b885ab8ddf0805bfe28be4b96845c79264
Saul Paredes <saulkurosaki13@gmail.com>
2024-04-30 19:00:32 -0600

    feat(AchievementsDetailsPage1): implement the DetailsHeader props

--- app/(root)/blog/metas-cumplidas/imc-manda-a-sus-artistas-a-nivel-internacional/page.tsx
+++ app/(root)/blog/metas-cumplidas/imc-manda-a-sus-artistas-a-nivel-internacional/page.tsx
@@ -1,9 +1,14 @@
 import DetailsHeader from "@/components/blog/blogDetailsPage/DetailsHeader";
+import { achievementsHeaders } from "@/lib/constants";
 
 const AchievementsDetailsPage1 = () => {
   return (
     <div className="relative bg-[#fff] w-full h-full max-md:pt-14 md:pt-[70px] xl:pt-14 lg:pt-14">
-      <DetailsHeader />
+      <DetailsHeader
+        title={achievementsHeaders[0].title}
+        date={achievementsHeaders[0].date}
+        image={achievementsHeaders[0].image}
+      />
     </div>
   );
 };

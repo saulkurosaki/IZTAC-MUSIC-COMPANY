146f36b6c067f80cea0dd78438e7f6ce93c34485
Saul Paredes <saulkurosaki13@gmail.com>
2024-04-30 18:32:48 -0600

    feat(AchievementsDetailsPage1): implement the AchievementsDetailsPage1 container layout styles

--- app/(root)/blog/metas-cumplidas/imc-manda-a-sus-artistas-a-nivel-internacional/page.tsx
+++ app/(root)/blog/metas-cumplidas/imc-manda-a-sus-artistas-a-nivel-internacional/page.tsx
@@ -1,5 +1,9 @@
 const AchievementsDetailsPage1 = () => {
-  return <div>AchievementsDetailsPage1 Test c:</div>;
+  return (
+    <div className="relative bg-[#fff] w-full h-full max-md:pt-14 md:pt-[70px] xl:pt-14 lg:pt-14">
+      AchievementsDetailsPage1 Test c:
+    </div>
+  );
 };
 
 export default AchievementsDetailsPage1;

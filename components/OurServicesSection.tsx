e13a3c95c99052cacecdf308ed36917f9de90ef8
Saul Paredes <saulkurosaki13@gmail.com>
2024-05-02 11:44:10 -0600

    fix(OurServicesSectin): fix little bugs

--- components/OurServicesSection.tsx
+++ components/OurServicesSection.tsx
@@ -2,7 +2,7 @@ import ServiceCard from "./home/ServiceCard";
 import { services } from "@/lib/constants";
 
 const OurServicesSection = () => (
-  <section className="w-full xl:h-[775px] lg:h-[714px] md:h-[2815px] max-md:h-[1995px] bg-[#B99253] py-8">
+  <section className="w-full h-auto bg-[#B99253] py-8">
     <div className="container xl:w-[1320px] lg:w-[960px] md:w-[720px] h-full px-0">
       <div className="flex w-full h-full max-lg:flex-col items-stretch">
         <ServiceCard

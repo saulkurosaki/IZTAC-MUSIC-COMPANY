7269d997500ac33e691c1438ce5d607933318fff
Saul Paredes <saulkurosaki13@gmail.com>
2024-05-01 19:18:06 -0600

    feat(RecognitionsDetailsPage2): implement the DetailsHeader complete component within the page

--- app/(root)/blog/reconocimientos/nuestra-liricista-gana-premio-estatal/page.tsx
+++ app/(root)/blog/reconocimientos/nuestra-liricista-gana-premio-estatal/page.tsx
@@ -1,7 +1,14 @@
+import DetailsHeader from "@/components/blog/blogDetailsPage/DetailsHeader";
+import { recognitionsInfo } from "@/lib/constants";
+
 const RecognitionsDetailsPage2 = () => {
   return (
     <div className="relative bg-[#fff] w-full h-full max-md:pt-14 md:pt-[70px] xl:pt-14 lg:pt-14">
-      RecognitionsDetailsPage2 component Test c:
+      <DetailsHeader
+        title={recognitionsInfo[1].title}
+        date={recognitionsInfo[1].date}
+        image={recognitionsInfo[1].image}
+      />
     </div>
   );
 };

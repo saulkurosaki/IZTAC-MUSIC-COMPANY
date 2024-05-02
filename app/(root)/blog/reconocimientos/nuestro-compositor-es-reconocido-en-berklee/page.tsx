28949ba9a85376bc988bba959ffcc7db82e13308
Saul Paredes <saulkurosaki13@gmail.com>
2024-05-01 19:26:59 -0600

    feat(RecognitionsDetailsPage3): implement the RecognitionsDetailsPage3 container layout styles

---
+++ app/(root)/blog/reconocimientos/nuestro-compositor-es-reconocido-en-berklee/page.tsx
@@ -0,0 +1,9 @@
+const RecognitionsDetailsPage3 = () => {
+  return (
+    <div className="relative bg-[#fff] w-full h-full max-md:pt-14 md:pt-[70px] xl:pt-14 lg:pt-14">
+      RecognitionsDetailsPage3 component Test c:
+    </div>
+  );
+};
+
+export default RecognitionsDetailsPage3;

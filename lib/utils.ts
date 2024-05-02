365b822719cf8ab73ec9ff4063a561a0990b8a61
Saul Paredes <saulkurosaki13@gmail.com>
2024-05-02 12:40:46 -0600

    fix(utils.ts): fix little generateClipPathBottomStyle styles bugs

--- lib/utils.ts
+++ lib/utils.ts
@@ -47,12 +47,12 @@ export const generateClipPathBottomStyle = (mediaQuery: number) => {
   if (mediaQuery <= 768) {
     return "polygon(0 52%, 100% 83%, 100% 100%, 0 100%)";
   } else if (mediaQuery <= 1024) {
-    return "polygon(0 28%, 100% 69%, 100% 100%, 0 100%)";
+    return "polygon(0 28%, 100% 69%, 100% 101%, 0 101%)";
   } else if (mediaQuery <= 1280) {
     return "polygon(0 31%, 100% 82%, 100% 100%, 0 100%)";
   } else if (mediaQuery <= 1536) {
-    return "polygon(0 26%, 100% 87%, 100% 100%, 0 100%)";
+    return "polygon(0 26%, 100% 87%, 100% 101%, 0 101%)";
   } else {
-    return "polygon(0 0, 100% 100%, 100% 100%, 0 100%)";
+    return "polygon(0 0, 100% 100%, 100% 101%, 0 101%)";
   }
 };

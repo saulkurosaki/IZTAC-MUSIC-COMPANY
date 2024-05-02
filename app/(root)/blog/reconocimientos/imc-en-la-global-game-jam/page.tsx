e53f92b4a2561476a4d552f3b992e3c3640611a2
Saul Paredes <saulkurosaki13@gmail.com>
2024-05-01 18:59:35 -0600

    feat(RecognitionsDetailsPage1): implement the RecognitionsDetailsPage1 container layout styles

--- app/(root)/blog/reconocimientos/imc-en-la-global-game-jam/page.tsx
+++ app/(root)/blog/reconocimientos/imc-en-la-global-game-jam/page.tsx
@@ -1,5 +1,9 @@
 const RecognitionsDetailsPage1 = () => {
-  return <div>RecognitionsDetailsPage1 component Test c:</div>;
+  return (
+    <div className="relative bg-[#fff] w-full h-full max-md:pt-14 md:pt-[70px] xl:pt-14 lg:pt-14">
+      RecognitionsDetailsPage1 component Test c:
+    </div>
+  );
 };
 
 export default RecognitionsDetailsPage1;

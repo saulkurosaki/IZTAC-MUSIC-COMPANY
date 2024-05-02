0e181e48bf452727f443d669be30ed3f3c99accd
Saul Paredes <saulkurosaki13@gmail.com>
2024-05-01 18:35:41 -0600

    feat(AchievementsDetailsPage4): implement the DetailsHeader complete component within the page

--- app/(root)/blog/metas-cumplidas/victor-consigue-papel-estelar-para-la-obra-in-the-heights/page.tsx
+++ app/(root)/blog/metas-cumplidas/victor-consigue-papel-estelar-para-la-obra-in-the-heights/page.tsx
@@ -1,7 +1,14 @@
+import DetailsHeader from "@/components/blog/blogDetailsPage/DetailsHeader";
+import { achievementsInfo } from "@/lib/constants";
+
 const AchievementsDetailsPage4 = () => {
   return (
     <div className="relative bg-[#fff] w-full h-full max-md:pt-14 md:pt-[70px] xl:pt-14 lg:pt-14">
-      AchievementsDetailsPage4 component Test c:
+      <DetailsHeader
+        title={achievementsInfo[3].title}
+        date={achievementsInfo[3].date}
+        image={achievementsInfo[3].image}
+      />
     </div>
   );
 };

f66ed5d709d3346cf16cf4e29d2a82d8e6671e4d
Saul Paredes <saulkurosaki13@gmail.com>
2024-05-01 18:34:36 -0600

    feat(AchievementsDetailsPage4): implement the AchievementsDetailsPage4 container layout styles

--- app/(root)/blog/metas-cumplidas/victor-consigue-papel-estelar-para-la-obra-in-the-heights/page.tsx
+++ app/(root)/blog/metas-cumplidas/victor-consigue-papel-estelar-para-la-obra-in-the-heights/page.tsx
@@ -1,5 +1,9 @@
 const AchievementsDetailsPage4 = () => {
-  return <div>AchievementsDetailsPage4 component Test c:</div>;
+  return (
+    <div className="relative bg-[#fff] w-full h-full max-md:pt-14 md:pt-[70px] xl:pt-14 lg:pt-14">
+      AchievementsDetailsPage4 component Test c:
+    </div>
+  );
 };
 
 export default AchievementsDetailsPage4;

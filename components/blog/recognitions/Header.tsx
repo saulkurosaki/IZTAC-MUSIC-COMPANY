f40610631e51648b85b67662226042bcfab5e606
Saul Paredes <saulkurosaki13@gmail.com>
2024-05-01 18:47:55 -0600

    fix little hover errors

--- components/blog/recognitions/Header.tsx
+++ components/blog/recognitions/Header.tsx
@@ -21,7 +21,7 @@ const Header = () => {
 
       <div className="absolute inset-0 py-12 z-20">
         <div className="container xl:w-[1320px] lg:w-[960px] md:w-[720px] max-md:w-full h-full px-[15px]">
-          <div className="w-full text-[#eee] hover:opacity-70 font-light oxygen mt-2">
+          <div className="w-full text-[#eee] font-light oxygen mt-2">
             <h1 className="lg:text-6xl md:text-5xl max-md:text-4xl">
               Reconocimientos
             </h1>

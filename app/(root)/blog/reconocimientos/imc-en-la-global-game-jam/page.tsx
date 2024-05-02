d1bc836d38da90cab126ea2ded987f1e0e2ff860
Saul Paredes <saulkurosaki13@gmail.com>
2024-05-01 19:00:49 -0600

    feat(RecognitionsDetailsPage1): implement the DetailsHeader complete component within the page

--- app/(root)/blog/reconocimientos/imc-en-la-global-game-jam/page.tsx
+++ app/(root)/blog/reconocimientos/imc-en-la-global-game-jam/page.tsx
@@ -1,7 +1,14 @@
+import DetailsHeader from "@/components/blog/blogDetailsPage/DetailsHeader";
+import { recognitionsInfo } from "@/lib/constants";
+
 const RecognitionsDetailsPage1 = () => {
   return (
     <div className="relative bg-[#fff] w-full h-full max-md:pt-14 md:pt-[70px] xl:pt-14 lg:pt-14">
-      RecognitionsDetailsPage1 component Test c:
+      <DetailsHeader
+        title={recognitionsInfo[0].title}
+        date={recognitionsInfo[0].date}
+        image={recognitionsInfo[0].image}
+      />
     </div>
   );
 };

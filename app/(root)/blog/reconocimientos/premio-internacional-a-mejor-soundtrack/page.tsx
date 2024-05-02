e1c43f55c6b04aefec6d824b75bb85827c47af04
Saul Paredes <saulkurosaki13@gmail.com>
2024-05-01 19:39:31 -0600

    feat(RecognitionsDetailsPage4): implement the DetailsHeader complete component within the page

--- app/(root)/blog/reconocimientos/premio-internacional-a-mejor-soundtrack/page.tsx
+++ app/(root)/blog/reconocimientos/premio-internacional-a-mejor-soundtrack/page.tsx
@@ -1,7 +1,14 @@
+import DetailsHeader from "@/components/blog/blogDetailsPage/DetailsHeader";
+import { recognitionsInfo } from "@/lib/constants";
+
 const RecognitionsDetailsPage4 = () => {
   return (
     <div className="relative bg-[#fff] w-full h-full max-md:pt-14 md:pt-[70px] xl:pt-14 lg:pt-14">
-      RecognitionsDetailsPage4 component Test c:
+      <DetailsHeader
+        title={recognitionsInfo[3].title}
+        date={recognitionsInfo[3].date}
+        image={recognitionsInfo[3].image}
+      />
     </div>
   );
 };

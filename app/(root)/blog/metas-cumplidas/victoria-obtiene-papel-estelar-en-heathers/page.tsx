bacc4020a5ba230a085eae7a351dca2accc14e20
Saul Paredes <saulkurosaki13@gmail.com>
2024-05-01 18:24:46 -0600

    feat(AchievementsDetailsPage3): implement the DetailsHeader complete component within the page

--- app/(root)/blog/metas-cumplidas/victoria-obtiene-papel-estelar-en-heathers/page.tsx
+++ app/(root)/blog/metas-cumplidas/victoria-obtiene-papel-estelar-en-heathers/page.tsx
@@ -1,7 +1,14 @@
+import DetailsHeader from "@/components/blog/blogDetailsPage/DetailsHeader";
+import { achievementsInfo } from "@/lib/constants";
+
 const AchievementsDetailsPage3 = () => {
   return (
     <div className="relative bg-[#fff] w-full h-full max-md:pt-14 md:pt-[70px] xl:pt-14 lg:pt-14">
-      AchievementsDetailsPage3 component Test c:
+      <DetailsHeader
+        title={achievementsInfo[2].title}
+        date={achievementsInfo[2].date}
+        image={achievementsInfo[2].image}
+      />
     </div>
   );
 };
